3aea1cb4d00cca5efd0bb24bd9f4604543866474
Nicolas Perriault <nperriault@gmail.com>
2015-12-24 15:25:49 +0100

    Initial live editor PoC.

--- app.js
+++ app.js
@@ -8,17 +8,98 @@ const schema = {
   }
 };
 
-const formData =  {
-  title: "First task",
-  done: true
+const uiSchema = {
+  done: {
+    widget: "radio"
+  }
+};
+
+const cmOptions = {
+  theme: "default",
+  mode: {
+    name: "javascript",
+    json: true,
+    statementIndent: 2,
+  },
+  lineNumbers: true,
+  indentWithTabs: false,
+  tabSize: 2,
 };
 
 const log = (type) => console.log.bind(console, type);
 
-ReactDOM.render((
-  <JSONSchemaForm schema={schema}
-        formData={formData}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")} />
-), document.getElementById("app"));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      valid: true,
+      schema,
+      schemaJson: JSON.stringify(schema, null, 2),
+      uiSchema,
+      uiSchemaJson: JSON.stringify(uiSchema, null, 2),
+    };
+  }
+
+  onJsonSchemaUpdate(schemaJson) {
+    try {
+      this.setState({
+        valid: true,
+        schema: JSON.parse(schemaJson, null, 2),
+        schemaJson
+      });
+    } catch(err) {
+      this.setState({
+        valid: false,
+        schemaJson
+      });
+    }
+  }
+
+  onUISchemaJsonUpdate(uiSchemaJson) {
+    try {
+      this.setState({
+        valid: true,
+        uiSchema: JSON.parse(uiSchemaJson, null, 2),
+        uiSchemaJson
+      });
+    } catch(err) {
+      this.setState({
+        valid: false,
+        uiSchemaJson
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="editor col-md-6">
+          <fieldset>
+            <legend>JSONSchema</legend>
+            <Codemirror
+              value={this.state.schemaJson}
+              onChange={this.onJsonSchemaUpdate.bind(this)}
+              options={cmOptions} />
+          </fieldset>
+          <fieldset>
+            <legend>UISchema</legend>
+            <Codemirror
+              value={this.state.uiSchemaJson}
+              onChange={this.onUISchemaJsonUpdate.bind(this)}
+              options={cmOptions} />
+          </fieldset>
+        </div>
+        <div className="col-md-6">
+          <JSONSchemaForm
+            schema={this.state.schema}
+            uiSchema={this.state.uiSchema}
+            onChange={log("changed")}
+            onSubmit={log("submitted")}
+            onError={log("errors")} />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
