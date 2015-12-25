a090b8a080213befa1a568fc19581bbf8f2c32c1
Nicolas Perriault <nperriault@gmail.com>
2015-12-25 23:06:17 +0100

    Multiple external json examples.

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
-const schema = {
-  title: "Todo Tasks",
-  type: "object",
-  required: ["title"],
-  properties: {
-    title: {type: "string", title: "Title", default: "A new task"},
-    done: {type: "boolean", title: "Done?", default: false}
-  }
-};
-
-const uiSchema = {
-  done: {
-    widget: "radio"
-  }
-};
-
-const formData = {
-  title: "My task",
-  done: false
-};
-
+const log = (type) => console.log.bind(console, type);
+const fromJson = (json) => JSON.parse(json);
+const toJson = (val) => JSON.stringify(val, null, 2);
 const cmOptions = {
   theme: "default",
+  height: "auto",
+  viewportMargin: Infinity,
   mode: {
     name: "javascript",
     json: true,
     statementIndent: 2,
   },
   lineNumbers: true,
+  lineWrapping: true,
   indentWithTabs: false,
   tabSize: 2,
 };
+const samples = {
+  Simple: "simple.json",
+  Nested: "nested.json",
+};
 
-const log = (type) => console.log.bind(console, type);
-const fromJson = (json) => JSON.parse(json);
-const toJson = (val) => JSON.stringify(val, null, 2);
+function loadSample(url) {
+  return fetch(url)
+    .then(res => res.json());
+}
 
 class Editor extends React.Component {
   constructor(props) {
@@ -73,20 +63,72 @@ class Editor extends React.Component {
   }
 }
 
-class App extends React.Component {
+class Selector extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {schema, uiSchema, formData};
+    this.state = {current: "Simple", loading: null};
+  }
+
+  onClick(label, url, event) {
+    event.preventDefault();
+    this.setState({loading: label});
+    this.props.onLoading();
+    loadSample(url)
+      .then(data => {
+        this.setState({current: label, loading: null});
+        this.props.onLoaded(data);
+      });
   }
 
   render() {
     return (
-      <div>
-        <div className="editor col-md-6">
+      <ul className="nav nav-pills">{
+        Object.keys(samples).map((label, i) => {
+          return (
+            <li role="presentation"
+              className={this.state.current === label ? "active" : ""}>
+              <a href={samples[label]}
+                onClick={this.onClick.bind(this, label, samples[label])}>
+                {this.state.loading === label ? "Loading..." : label}
+              </a>
+            </li>
+          );
+        })
+      }</ul>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: false, schema: {}, uiSchema: {}, formData: {}};
+  }
+
+  componentDidMount() {
+    this.setState({loading: true});
+    loadSample("simple.json")
+      .then(data => this.loaded(data));
+  }
+
+  loaded(data) {
+    this.setState(Object.assign({}, data, {loading: false}));
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="page-header">
+          <h1>react-jsonschema-form</h1>
+          <Selector
+            onLoading={_ => this.setState({loading: true})}
+            onLoaded={(data) => this.loaded(data)} />
+        </div>
+        <div className="col-md-6">
           <Editor title="JSONSchema"
             code={toJson(this.state.schema)}
             onChange={schema => this.setState({schema})} />
-          <div>
+          <div className="row">
             <div className="col-md-6">
               <Editor title="UISchema"
                 code={toJson(this.state.uiSchema)}
@@ -100,13 +142,14 @@ class App extends React.Component {
           </div>
         </div>
         <div className="col-md-6">
-          <JSONSchemaForm
-            schema={this.state.schema}
-            uiSchema={this.state.uiSchema}
-            formData={this.state.formData}
-            onChange={data => this.setState({formData: data.formData})}
-            onSubmit={data => this.setState({formData: data.formData})}
-            onError={log("errors")} />
+          {this.state.loading ? null :
+            <JSONSchemaForm
+              schema={this.state.schema}
+              uiSchema={this.state.uiSchema}
+              formData={this.state.formData}
+              onChange={data => this.setState({formData: data.formData})}
+              onSubmit={data => this.setState({formData: data.formData})}
+              onError={log("errors")} />}
         </div>
       </div>
     );
