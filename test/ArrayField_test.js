d31e42915d43140aa78befd9f0cd6d6a49b71d40
王勐喆 <rhgb@users.noreply.github.com>
2016-04-11 11:06:45 +0800

    add tests

--- test/ArrayField_test.js
+++ test/ArrayField_test.js
@@ -214,4 +214,67 @@ describe("ArrayField", () => {
       expect(node.querySelectorAll("fieldset fieldset")).to.have.length.of(1);
     });
   });
+
+  describe("Fixed items lists", () => {
+    const schema = {
+      type: "array",
+      title: "List of fixed items",
+      items: [
+        {
+          type: "string",
+          title: "Some text"
+        },
+        {
+          type: "number",
+          title: "A number"
+        }
+      ]
+    };
+
+    it("should render a fieldset", () => {
+      const {node} = createFormComponent({schema});
+
+      expect(node.querySelectorAll("fieldset"))
+          .to.have.length.of(1);
+    });
+
+    it("should render a fieldset legend", () => {
+      const {node} = createFormComponent({schema});
+
+      expect(node.querySelector("fieldset > legend").textContent)
+          .eql("List of fixed items");
+    });
+
+    it("should render field widgets", () => {
+      const {node} = createFormComponent({schema});
+      const strInput = node.querySelector("fieldset .field-string input[type=text]");
+      const numInput = node.querySelector("fieldset .field-number input[type=text]");
+      expect(strInput.id).eql("root_0");
+      expect(numInput.id).eql("root_1");
+    });
+
+    it("should fill fields with data", () => {
+      const {node} = createFormComponent({schema, formData: ["foo", 42]});
+      const strInput = node.querySelector("fieldset .field-string input[type=text]");
+      const numInput = node.querySelector("fieldset .field-number input[type=text]");
+      expect(strInput.value).eql("foo");
+      expect(numInput.value).eql("42");
+    });
+
+    it("should handle change events", () => {
+      const {comp, node} = createFormComponent({schema});
+      const strInput = node.querySelector("fieldset .field-string input[type=text]");
+      const numInput = node.querySelector("fieldset .field-number input[type=text]");
+
+      Simulate.change(strInput, {
+        target: { value: "bar" }
+      });
+      Simulate.change(numInput, {
+        target: { value: "101" }
+      });
+
+      expect(comp.state.formData).eql(["bar", 101]);
+    });
+
+  });
 });
