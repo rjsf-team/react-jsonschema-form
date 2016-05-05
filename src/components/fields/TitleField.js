7560a64661608c0ac63c62df05341ce918b41896
Nicolas Perriault <nperriault@gmail.com>
2016-05-05 11:41:58 +0200

    Fixes #186: Array and Object field TitleField ids.

--- src/components/fields/TitleField.js
+++ src/components/fields/TitleField.js
@@ -3,15 +3,16 @@ import React, {PropTypes} from "react";
 const REQUIRED_FIELD_SYMBOL = "*";
 
 function TitleField(props) {
-  const {title, required} = props;
+  const {id, title, required} = props;
   const legend = required ? title + REQUIRED_FIELD_SYMBOL : title;
-  return <legend>{legend}</legend>;
+  return <legend id={id}>{legend}</legend>;
 }
 
 if (process.env.NODE_ENV !== "production") {
   TitleField.propTypes = {
+    id: PropTypes.string,
     title: PropTypes.string,
-    required: PropTypes.bool
+    required: PropTypes.bool,
   };
 }
 
