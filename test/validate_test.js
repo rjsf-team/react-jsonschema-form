c225c9974b0e35fde5a282bccf6e0e5d7b7179e2
Nicolas Perriault <nperriault@gmail.com>
2016-05-09 12:48:36 +0200

    Refactored validate tests.

--- test/validate_test.js
+++ test/validate_test.js
@@ -1,43 +1,69 @@
 import { expect } from "chai";
-import sinon from "sinon";
 
 import validateFormData from "../src/validate";
 
 
 describe("validateFormData()", () => {
-  const schema = {
-    type: "object",
-    properties: {
-      foo: {type: "string"}
-    }
-  };
+  describe("No custom validate function", () => {
+    const schema = {
+      type: "object",
+      properties: {
+        foo: {type: "string"}
+      }
+    };
 
-  it("should return errors and errorSchema properties", () => {
-    const formData = {};
-    const validate = (formData, errors) => errors;
-
-    const result = validateFormData(formData, schema, validate);
-
-    expect(result).eql({errors: [], errorSchema: {__errors: []}});
-  });
-
-  describe("wrapped formData", () => {
-    let errorHandler;
+    let errors, errorSchema;
 
     beforeEach(() => {
-      const formData = {a: {b: 42}};
-      const validate = sinon.stub().returns({});
-
-      validateFormData(formData, schema, validate);
-      errorHandler = validate.getCall(0).args[1];
+      const result = validateFormData({foo: 42}, schema);
+      errors = result.errors;
+      errorSchema = result.errorSchema;
     });
 
-    it("should implement addError", () => {
-      expect(errorHandler).to.include.key("addError");
-      expect(errorHandler).to.include.key("a");
-      expect(errorHandler.a).to.include.key("addError");
-      expect(errorHandler.a).to.include.key("b");
-      expect(errorHandler.a.b).to.include.key("addError");
+    it("should return an error list", () => {
+      expect(errors).to.have.length.of(1);
+      expect(errors[0].message).eql("is not of a type(s) string");
+    });
+
+    it("should return an errorSchema", () => {
+      expect(errorSchema.foo.__errors).to.have.length.of(1);
+      expect(errorSchema.foo.__errors[0]).eql("is not of a type(s) string");
+    });
+  });
+
+  describe("Custom validate function", () => {
+    let errors, errorSchema;
+
+    const schema = {
+      type: "object",
+      required: ["pass1", "pass2"],
+      properties: {
+        pass1: {type: "string"},
+        pass2: {type: "string"},
+      }
+    };
+
+    beforeEach(() => {
+      const validate = (formData, errors) => {
+        if (formData.pass1 !== formData.pass2) {
+          errors.pass2.addError("passwords don't match.");
+        }
+        return errors;
+      };
+      const formData = {pass1: "a", pass2: "b"};
+      const result = validateFormData(formData, schema, validate);
+      errors = result.errors;
+      errorSchema = result.errorSchema;
+    });
+
+    it("should return an error list", () => {
+      expect(errors).to.have.length.of(1);
+      expect(errors[0].stack).eql("pass2 passwords don't match.");
+    });
+
+    it("should return an errorSchema", () => {
+      expect(errorSchema.pass2.__errors).to.have.length.of(1);
+      expect(errorSchema.pass2.__errors[0]).eql("passwords don't match.");
     });
   });
 });
