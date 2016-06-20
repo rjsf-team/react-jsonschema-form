5c452781d8c98ff2929cd805f66603733036fae1
Ollie Relph <ollie@relph.me>
2016-06-20 15:00:12 +0100

    Add failing test

--- test/utils_test.js
+++ test/utils_test.js
@@ -245,6 +245,15 @@ describe("utils", () => {
     it("should return the raw value if the input ends with a dot", () => {
       expect(asNumber("3.")).eql("3.");
     });
+
+    it("should not convert the value to an integer if the input ends with a 0", () => {
+      // this is to allow users to input 3.07
+      expect(asNumber("3.0")).eql("3.0");
+    });
+
+    it("should allow numbers with a 0 in the first decimal place", () => {
+      expect(asNumber("3.07")).eql(3.07);
+    });
   });
 
   describe("isMultiSelect()", () => {
