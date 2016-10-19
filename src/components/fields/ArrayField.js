f16c111c2fd9fde76bb429f80f4180d0747366b4
Olli Raitio <olli.raitio@helsinki.fi>
2016-10-19 12:32:12 +0300

    Some code style improvement

--- src/components/fields/ArrayField.js
+++ src/components/fields/ArrayField.js
@@ -339,16 +339,12 @@ class ArrayField extends Component {
     const {SchemaField} = this.props.registry.fields;
     const {disabled, readonly, uiSchema} = this.props;
 
-    let orderable = true;
-    const options = uiSchema["ui:options"];
-    if (options && options.hasOwnProperty("orderable")) {
-      orderable = !!options.orderable;
-    }
-    if (!orderable) {
-      canMoveUp = false;
-      canMoveDown = false;
-    }
-    const hasToolbar = removable || canMoveUp || canMoveDown;
+	  const {orderable} = {orderable: true, ...uiSchema["ui:options"]};
+
+	  const _canMoveUp = orderable && canMoveUp;
+	  const _canMoveDown = orderable && canMoveDown;
+
+    const hasToolbar = removable || _canMoveUp || _canMoveDown;
 
     const btnStyle = {flex: 1, paddingLeft: 6, paddingRight: 6, fontWeight: "bold"};
 
@@ -372,18 +368,18 @@ class ArrayField extends Component {
           hasToolbar ?
             <div className="col-xs-2 array-item-toolbox text-right">
               <div className="btn-group" style={{display: "flex"}}>
-                {canMoveUp || canMoveDown ?
+                {_canMoveUp || _canMoveDown ?
                   <button type="button" className="btn btn-default array-item-move-up"
                           style={btnStyle}
                           tabIndex="-1"
-                          disabled={disabled || readonly || !canMoveUp}
+                          disabled={disabled || readonly || !_canMoveUp}
                           onClick={this.onReorderClick(index, index - 1)}>⬆</button>
                   : null}
-                {canMoveUp || canMoveDown ?
+                {_canMoveUp || _canMoveDown ?
                   <button type="button" className="btn btn-default array-item-move-down"
                           style={btnStyle}
                           tabIndex="-1"
-                          disabled={disabled || readonly || !canMoveDown}
+                          disabled={disabled || readonly || !_canMoveDown}
                           onClick={this.onReorderClick(index, index + 1)}>⬇</button>
                   : null}
                 {removable ?
