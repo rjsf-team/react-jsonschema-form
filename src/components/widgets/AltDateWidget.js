a976673a49f705c345e34330082a8ea756fa209e
Eric Lee <eric.lee@interface48.com>
2016-11-15 23:12:18 -0800

    Revert AltDateWidget back to commit 547344905da6e9b78bda7f42341df40ae38584ea

--- src/components/widgets/AltDateWidget.js
+++ src/components/widgets/AltDateWidget.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from "react";
 
 import {shouldRender, parseDateString, toDateString, pad} from "../../utils";
+import SelectWidget from "../widgets/SelectWidget";
 
 
 function rangeOptions(type, start, stop) {
@@ -16,9 +17,8 @@ function readyForChange(state) {
 }
 
 function DateElement(props) {
-  const {type, range, value, select, rootId, disabled, readonly, autofocus, registry} = props;
+  const {type, range, value, select, rootId, disabled, readonly, autofocus} = props;
   const id = rootId + "_" + type;
-  const {SelectWidget} = registry.widgets;
   return (
     <SelectWidget
       schema={{type: "integer"}}
@@ -101,7 +101,7 @@ class AltDateWidget extends Component {
   }
 
   render() {
-    const {id, disabled, readonly, autofocus, registry} = this.props;
+    const {id, disabled, readonly, autofocus} = this.props;
     return (
       <ul className="list-inline">{
         this.dateElementProps.map((elemProps, i) => (
@@ -112,7 +112,6 @@ class AltDateWidget extends Component {
               {...elemProps}
               disabled= {disabled}
               readonly={readonly}
-              registry={registry}
               autofocus={autofocus && i === 0}/>
           </li>
         ))
