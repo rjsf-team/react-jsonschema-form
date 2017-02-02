d1f5f188025f8aa7dadbff45490aefed4e349598
mc_fish <oribicic@gmail.com>
2017-02-02 12:19:20 +0100

    cleanup

--- src/utils.js
+++ src/utils.js
@@ -179,22 +179,6 @@ export function isObject(thing) {
   return typeof thing === "object" && thing !== null && !Array.isArray(thing);
 }
 
-// export function mergeObjects(obj1, obj2, concatArrays = false) {
-//   // Recursively merge deeply nested objects.
-//   var acc = Object.assign({}, obj1); // Prevent mutation of source object.
-//   return Object.keys(obj2).reduce((acc, key) =>{
-//     const left = obj1[key], right = obj2[key];
-//     if (obj1.hasOwnProperty(key) && isObject(right)) {
-//       acc[key] = mergeObjects(left, right, concatArrays);
-//     } else if (concatArrays && Array.isArray(left) && Array.isArray(right)) {
-//       acc[key] = left.concat(right);
-//     } else {
-//       acc[key] = right;
-//     }
-//     return acc;
-//   }, acc);
-// }
-
 export function asNumber(value) {
   if (value === "") {
     return undefined;
