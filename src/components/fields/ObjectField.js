890f970871dd595d4b7010b2bac1614a3c883ed7
Olli Raitio <olli.raitio@helsinki.fi>
2017-10-03 12:48:53 +0300

    Ran cs-format & cs-check

--- src/components/fields/ObjectField.js
+++ src/components/fields/ObjectField.js
@@ -12,13 +12,13 @@ function DefaultObjectFieldTemplate(props) {
   return (
     <fieldset>
       {(props.uiSchema["ui:title"] || props.title) && (
-          <TitleField
-            id={`${props.idSchema.$id}__title`}
-            title={props.title || props.uiSchema["ui:title"]}
-            required={props.required}
-            formContext={props.formContext}
-          />
-        )}
+        <TitleField
+          id={`${props.idSchema.$id}__title`}
+          title={props.title || props.uiSchema["ui:title"]}
+          required={props.required}
+          formContext={props.formContext}
+        />
+      )}
       {props.description && (
         <DescriptionField
           id={`${props.idSchema.$id}__description`}
