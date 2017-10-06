ce8a46a03b186d7563a7c1af180b95353a1ad8c4
Jacob Selig <jselig@fanatics.com>
2017-10-06 15:58:47 -0600

    fixes #622

--- src/components/widgets/DateTimeWidget.js
+++ src/components/widgets/DateTimeWidget.js
@@ -1,8 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+function pad(p, n) {
+  return String(n).padStart(p, 0);
+}
+
 function fromJSONDate(jsonDate) {
-  return jsonDate ? jsonDate.slice(0, 19) : "";
+  if (!jsonDate) {
+    return "";
+  }
+
+  // required format of `"yyyy-MM-ddThh:mm" followed by optional ":ss" or ":ss.SSS"
+  // https://html.spec.whatwg.org/multipage/input.html#local-date-and-time-state-(type%3Ddatetime-local)
+  // > should be a _valid local date and time string_ (not GMT)
+
+  const date = Date.parse(jsonDate);
+
+  const yyyy = pad(4, date.getFullYear());
+  const MM = pad(2, date.getMonth() + 1);
+  const dd = pad(2, date.getDate());
+  const hh = pad(2, date.getHours());
+  const mm = pad(2, date.getMinutes());
+  const ss = pad(2, date.getSeconds());
+  const SSS = pad(3, date.getMilliseconds());
+
+  return `${yyyy}-${MM}-${dd}T${hh}:${mm}:${ss}.${SSS}`;
 }
 
 function toJSONDate(dateString) {
