76b57e9387bb81b2df275a8ecb039ed89bfb4ca5
Danil Zhelnov <danil.zhelnov@graph.uk>
2017-10-13 14:10:15 -0400

    Fix customObject sample (#716)
    The sample for customObject.js had few defects. React 15.6.1 throws an
    no-keys-defined warning as well as 'child-can-not-be-objects'
    exception. The code has been changed to fix these issues.
    
    * Fixed customObject sample to fix "Objects are not valid as a React child" error for react 15.6.1 and to avoid "https://fb.me/react-warning-keys" warning
    
    * Run prettier

--- playground/samples/customObject.js
+++ playground/samples/customObject.js
@@ -6,7 +6,11 @@ function ObjectFieldTemplate({ TitleField, properties, title, description }) {
       <TitleField title={title} />
       <div className="row">
         {properties.map(prop => (
-          <div className="col-lg-2 col-md-4 col-sm-6 col-xs-12">{prop}</div>
+          <div
+            className="col-lg-2 col-md-4 col-sm-6 col-xs-12"
+            key={prop.content.key}>
+            {prop.content}
+          </div>
         ))}
       </div>
       {description}
