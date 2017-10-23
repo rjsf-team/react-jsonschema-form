6152e34d65dbc83a01ae112ba7a095a8b910c341
mmmveggies <mmmveggies@users.noreply.github.com>
2017-10-23 08:39:14 -0600

    Fix #622 - correctly parse JSON date into local date (#715)
    * fixes #622
    
    * test update
    
    * pad years too, just in caseys
    
    * PR naming fixes

--- src/components/widgets/DateTimeWidget.js
+++ src/components/widgets/DateTimeWidget.js
@@ -1,11 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { pad } from "../../utils";
 
-function fromJSONDate(jsonDate) {
-  return jsonDate ? jsonDate.slice(0, 19) : "";
+export function utcToLocal(jsonDate) {
+  if (!jsonDate) {
+    return "";
+  }
+
+  // required format of `"yyyy-MM-ddThh:mm" followed by optional ":ss" or ":ss.SSS"
+  // https://html.spec.whatwg.org/multipage/input.html#local-date-and-time-state-(type%3Ddatetime-local)
+  // > should be a _valid local date and time string_ (not GMT)
+
+  // Note - date constructor passed local ISO-8601 does not correctly
+  // change time to UTC in node pre-8
+  const date = new Date(jsonDate);
+
+  const yyyy = pad(date.getFullYear(), 4);
+  const MM = pad(date.getMonth() + 1, 2);
+  const dd = pad(date.getDate(), 2);
+  const hh = pad(date.getHours(), 2);
+  const mm = pad(date.getMinutes(), 2);
+  const ss = pad(date.getSeconds(), 2);
+  const SSS = pad(date.getMilliseconds(), 3);
+
+  return `${yyyy}-${MM}-${dd}T${hh}:${mm}:${ss}.${SSS}`;
 }
 
-function toJSONDate(dateString) {
+export function localToUTC(dateString) {
   if (dateString) {
     return new Date(dateString).toJSON();
   }
@@ -17,8 +38,8 @@ function DateTimeWidget(props) {
     <BaseInput
       type="datetime-local"
       {...props}
-      value={fromJSONDate(value)}
-      onChange={value => onChange(toJSONDate(value))}
+      value={utcToLocal(value)}
+      onChange={value => onChange(localToUTC(value))}
     />
   );
 }
