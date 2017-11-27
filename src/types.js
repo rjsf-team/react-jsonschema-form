1ab120a9698c514eb602b9adf25b3f5c76a253fc
Anatoly Leskovets <aleskov2@wiley.com>
2017-11-27 13:52:20 +0300

    Registry shape defined.

---
+++ src/types.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+const { func, object, objectOf, oneOfType, shape } = PropTypes;
+
+export const registryShape = shape({
+  widgets: objectOf(oneOfType([func, object])).isRequired,
+  fields: objectOf(func).isRequired,
+  definitions: object.isRequired,
+  formContext: object.isRequired,
+  ArrayFieldTemplate: func,
+  ObjectFieldTemplate: func,
+  FieldTemplate: func
+});
