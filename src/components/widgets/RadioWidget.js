52097e23825793380f0ce737efa5cb94cdca754d
David Barratt <david@davidwbarratt.com>
2018-02-25 12:27:29 -0500

    Update Radio Widget to use Bootstrap v4

--- src/components/widgets/RadioWidget.js
+++ src/components/widgets/RadioWidget.js
@@ -14,17 +14,18 @@ function RadioWidget(props) {
   // Generating a unique field name to identify this set of radio buttons
   const name = Math.random().toString();
   const { enumOptions, inline } = options;
+  const inlineCls = inline ? "form-check-inline" : "";
   // checked={checked} has been moved above name={name}, As mentioned in #349;
   // this is a temporary fix for radio button rendering bug in React, facebook/react#7630.
   return (
-    <div className="field-radio-group">
+    <div className={`form-check ${inlineCls}`}>
       {enumOptions.map((option, i) => {
         const checked = option.value === value;
-        const disabledCls = disabled || readonly ? "disabled" : "";
         const radio = (
           <span>
             <input
               type="radio"
+              className="form-check-input"
               checked={checked}
               name={name}
               required={required}
@@ -37,13 +38,9 @@ function RadioWidget(props) {
           </span>
         );
 
-        return inline ? (
-          <label key={i} className={`radio-inline ${disabledCls}`}>
-            {radio}
-          </label>
-        ) : (
-          <div key={i} className={`radio ${disabledCls}`}>
-            <label>{radio}</label>
+        return (
+          <div key={i}>
+            <label className="form-check-label">{radio}</label>
           </div>
         );
       })}
