c0b11d534bf83b667bdcdcb2ea695504a93bb7c1
Matej Mazur <matej.mazur@mail.com>
2018-06-21 14:09:28 +0200

    Update test utils

--- tests/with-bootstrap/test_utils.js
+++ tests/with-bootstrap/test_utils.js
@@ -1,32 +1,17 @@
 /* Utils for tests. */
 
 import React from 'react';
-import sinon from 'sinon';
-import { renderIntoDocument } from 'react-addons-test-utils';
-import { findDOMNode, render } from 'react-dom';
+import { render } from 'react-testing-library';
 
-import Form from '../../packages/react-jsonschema-form/src';
+import Form from 'react-jsonschema-form/src';
 
 export function createComponent(Component, props) {
-  const comp = renderIntoDocument(<Component {...props} />);
-  const node = findDOMNode(comp);
-  return { comp, node };
+  const spy = jest.spyOn(Component.prototype, 'setState');
+  const utils = render(<Component {...props} />);
+  const comp = spy.mock.instances[0];
+  return { ...utils, comp };
 }
 
 export function createFormComponent(props) {
   return createComponent(Form, { ...props, safeRenderCompletion: true });
-}
-
-export function createSandbox() {
-  const sandbox = sinon.sandbox.create();
-  // Ensure we catch any React warning and mark them as test failures.
-  sandbox.stub(console, 'error', error => {
-    throw new Error(error);
-  });
-  return sandbox;
-}
-
-export function setProps(comp, newProps) {
-  const node = findDOMNode(comp);
-  render(React.createElement(comp.constructor, newProps), node.parentNode);
 }
