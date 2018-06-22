d94452aeaa4a9a139f66ccbf8604876027efbc32
Matej Mazur <matej.mazur@mail.com>
2018-06-22 12:14:29 +0200

    Update test utils

--- tests/with-bootstrap/test_utils.js
+++ tests/with-bootstrap/test_utils.js
@@ -7,10 +7,31 @@ import 'dom-testing-library/extend-expect';
 import Form from 'react-jsonschema-form/src';
 
 export function createComponent(Component, props) {
-  const spy = jest.spyOn(Component.prototype, 'setState');
+  const idPrefix = props.idPrefix || 'root';
+  const spy = jest.spyOn(Component.prototype, 'render');
   const utils = render(<Component {...props} />);
-  const comp = spy.mock.instances[0];
-  return { ...utils, comp };
+  const node = utils.container.firstChild;
+  return { ...utils, queryByModel, getInstance, rerender, node };
+
+  /**
+   * It converts model to data-testid and uses queryByTestId
+   ```
+   queryByModel('foo.bar') => queryByTestId('root_foo_bar')
+   ```
+   * @param {string} model - JSON Schema model (path with dot)
+   */
+  function queryByModel(model) {
+    const id = `${idPrefix}_${model.replace(/\./g, '_')}`;
+    return utils.queryByTestId(id);
+  }
+
+  function getInstance() {
+    return spy.mock.instances[spy.mock.instances.length - 1];
+  }
+
+  function rerender(newProps) {
+    utils.rerender(<Component {...props} {...newProps} />);
+  }
 }
 
 export function createFormComponent(props) {
