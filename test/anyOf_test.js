3933c5071f9961931a6865ab328b0eb4c5dd88bc
Lucian <lucian.buzzo@gmail.com>
2019-01-16 16:59:58 +0000

    Add test and update documentation for using anyOf inside array items
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- test/anyOf_test.js
+++ test/anyOf_test.js
@@ -299,4 +299,48 @@ describe("anyOf", () => {
 
     expect(node.querySelector("select").value).eql("1");
   });
+
+  describe("Arrays", () => {
+    it("should correctly render form inputs for anyOf inside array items", () => {
+      const schema = {
+        type: "object",
+        properties: {
+          items: {
+            type: "array",
+            items: {
+              type: "object",
+              anyOf: [
+                {
+                  properties: {
+                    foo: {
+                      type: "string",
+                    },
+                  },
+                },
+                {
+                  properties: {
+                    bar: {
+                      type: "string",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      };
+
+      const { node } = createFormComponent({
+        schema,
+      });
+
+      expect(node.querySelector(".array-item-add button")).not.eql(null);
+
+      Simulate.click(node.querySelector(".array-item-add button"));
+
+      expect(node.querySelectorAll("select")).to.have.length.of(1);
+
+      expect(node.querySelectorAll("input#root_foo")).to.have.length.of(1);
+    });
+  });
 });
