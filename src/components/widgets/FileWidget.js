57cacddd2502f39ddd18ebf141af14c28d1eed17
jorgerobles <jorge.diego.robles@gmail.com>
2019-01-19 21:00:28 +0100

    Update FileWidget.js

--- src/components/widgets/FileWidget.js
+++ src/components/widgets/FileWidget.js
@@ -4,8 +4,7 @@ import PropTypes from "prop-types";
 import { dataURItoBlob, shouldRender, setState } from "../../utils";
 
 function addNameToDataURL(dataURL, name) {
-  name=encodeURIComponent(name)
-  return dataURL.replace(";base64", `;name=${name};base64`);
+    return dataURL.replace(";base64", `;name=${encodeURIComponent(name)};base64`); 
 }
 
 function processFile(file) {
