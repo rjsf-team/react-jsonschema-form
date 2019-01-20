820552da0b2fcac2f99034e6365e4cdb0c9e7b8d
Jorge Robles <jorge.diego.robles@gmail.com>
2019-01-20 17:23:01 +0100

    Added Test

--- test/StringField_test.js
+++ test/StringField_test.js
@@ -1543,6 +1543,34 @@ describe("StringField", () => {
 
       Simulate.change(node.querySelector("[type=file]"), {
         target: {
+          files: [{ name: "file1.txt", size: 1, type: "type" }],
+        },
+      });
+
+      return new Promise(setImmediate).then(() =>
+        expect(comp.state.formData).eql(
+          "data:text/plain;name=file1.txt;base64,x="
+        )
+      );
+    });
+
+    it("should encode file name with encodeURIComponent", () => {
+      sandbox.stub(window, "FileReader").returns({
+        set onload(fn) {
+          fn({ target: { result: "data:text/plain;base64,x=" } });
+        },
+        readAsDataUrl() {},
+      });
+
+      const { comp, node } = createFormComponent({
+        schema: {
+          type: "string",
+          format: "data-url",
+        },
+      });
+
+      Simulate.change(node.querySelector("[type=file]"), {
+        target: {
           files: [{ name: nonUriEncodedValue, size: 1, type: "type" }],
         },
       });
