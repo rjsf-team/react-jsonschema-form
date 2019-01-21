c6c4cb5cc34a0bfedc21ff101e072935f01ff04f
sobia.adil <sobia.adil@go-jek.com>
2019-01-21 16:09:21 +0530

    Add custom styles for TextArea

--- src/components/widgets/TextareaWidget.js
+++ src/components/widgets/TextareaWidget.js
@@ -1,5 +1,22 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+const Textarea = styled.textarea`
+display: block;
+width: 100%;
+height: 34px;
+padding: 6px 12px;
+font-size: 14px;
+line-height: 1.42857143;
+color: #555;
+background-col
+or: #fff;
+background-image: none;
+border: 1px solid ${props => props.theme.borderColor};
+&:hover, &:focus {
+  border-color: ${props => props.theme.activeColor};
+`;
 
 function TextareaWidget(props) {
   const {
@@ -13,16 +30,16 @@ function TextareaWidget(props) {
     autofocus,
     onChange,
     onBlur,
-    onFocus,
+    onFocus
   } = props;
   const _onChange = ({ target: { value } }) => {
-    return onChange(value === "" ? options.emptyValue : value);
+    return onChange(value === '' ? options.emptyValue : value);
   };
   return (
-    <textarea
+    <Textarea
       id={id}
       className="form-control"
-      value={typeof value === "undefined" ? "" : value}
+      value={typeof value === 'undefined' ? '' : value}
       placeholder={placeholder}
       required={required}
       disabled={disabled}
@@ -38,16 +55,16 @@ function TextareaWidget(props) {
 
 TextareaWidget.defaultProps = {
   autofocus: false,
-  options: {},
+  options: {}
 };
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   TextareaWidget.propTypes = {
     schema: PropTypes.object.isRequired,
     id: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
     options: PropTypes.shape({
-      rows: PropTypes.number,
+      rows: PropTypes.number
     }),
     value: PropTypes.string,
     required: PropTypes.bool,
@@ -56,7 +73,7 @@ if (process.env.NODE_ENV !== "production") {
     autofocus: PropTypes.bool,
     onChange: PropTypes.func,
     onBlur: PropTypes.func,
-    onFocus: PropTypes.func,
+    onFocus: PropTypes.func
   };
 }
 
