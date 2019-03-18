a12e5b5bfbfcc48a77e341347ab23b62fa733d24
Ajit Bhaik <bag3557@gmail.com>
2019-03-18 14:54:04 +0530

    Fix: Custom Chip text
    Fix: Custom Chip text

--- src/components/widgets/AsyncMultiselectDropdown/SelectionBar.js
+++ src/components/widgets/AsyncMultiselectDropdown/SelectionBar.js
@@ -7,14 +7,17 @@ const SelectionBar = props => {
     selectedOptions,
     selectionColumn,
     onDeleteChoice,
+    primaryColumn,
+    getChipDisplayText
   } = props;
+  
   if (isMultiselect) {
     return (
       <React.Fragment>
         {selectedOptions.map((value, key) => (
           <Chip
             key={key}
-            label={value[selectionColumn]}
+            label={getChipDisplayText ? getChipDisplayText(value[primaryColumn]) : value[selectionColumn]}
             style={{ marginRight: 5, height: 30 }}
             onDelete={() => onDeleteChoice(value[selectionColumn])}
           />
@@ -24,7 +27,7 @@ const SelectionBar = props => {
   } else if (selectedOptions[0]) {
     return (
       <Chip
-        label={selectedOptions[0][selectionColumn]}
+        label={getChipDisplayText ? getChipDisplayText(selectedOptions[0][primaryColumn]): selectedOptions[0][selectionColumn]}
         style={{ marginRight: 5, height: 30 }}
         onDelete={() => onDeleteChoice(selectedOptions[0][selectionColumn])}
       />
