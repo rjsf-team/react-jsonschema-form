c5e8899701279a2ab1ac75551824ff24b8d187a4
faissalMT <41294831+faissalMT@users.noreply.github.com>
2019-04-12 21:35:05 +0100

    Add NullField (#1238)
    * Add NullField
    
    * Always set the formData to null for a null field
    
    * Don't overwrite existing data
    
    * Use central fields propTypes

---
+++ playground/samples/null.js
@@ -0,0 +1,28 @@
+module.exports = {
+  schema: {
+    title: "Null field example",
+    description: "A short form with a null field",
+    type: "object",
+    required: ["firstName"],
+    properties: {
+      helpText: {
+        title: "A null field",
+        description:
+          "Null fields like this are great for adding extra information",
+        type: "null",
+      },
+      firstName: {
+        type: "string",
+        title: "A regular string field",
+        default: "Chuck",
+      },
+    },
+  },
+  uiSchema: {
+    firstName: {
+      "ui:autofocus": true,
+      "ui:emptyValue": "",
+    },
+  },
+  formData: {},
+};
