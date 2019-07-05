60fefabdeee60f54c2b558b4fd8e7c15187ab0af
Miraj Patel <mirajp@users.noreply.github.com>
2019-07-04 22:47:20 -0700

    Address PR comment about race condition

--- src/components/Form.js
+++ src/components/Form.js
@@ -140,8 +140,7 @@ export default class Form extends Component {
       };
     }
 
-    this.setState(state);
-    this.props.onChange && this.props.onChange(state);
+    this.setState(state, () => this.props.onChange && this.props.onChange(state) );
   };
 
   onBlur = (...args) => {
