63c6127547c0e18584108f50008c4f8c6309d1ca
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-07-30 22:45:57 -0700

    fix: fix webpack public path for playground (#1379)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "build"),
     filename: "bundle.js",
-    publicPath: "/static/"
+    publicPath: "/react-jsonschema-form/"
   },
   plugins: [
     new MonacoWebpackPlugin({
