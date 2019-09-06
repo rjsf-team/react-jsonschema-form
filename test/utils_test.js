e483970cb1fcfd37d55e72b844906239d4b074c1
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-09-06 05:49:21 +0000

    unskip react 16 test

--- test/utils_test.js
+++ test/utils_test.js
@@ -2784,8 +2784,7 @@ describe("utils", () => {
       );
     });
 
-    //TODO: Unskip the test when react>=16.3 will be used
-    it.skip("should not fail on forwarded ref component", () => {
+    it("should not fail on forwarded ref component", () => {
       const Widget = React.forwardRef((props, ref) => (
         <div {...props} ref={ref} />
       ));
