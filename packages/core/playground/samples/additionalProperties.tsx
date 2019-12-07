35bb61c4e54e1de50650603b118e17cf006a50e1
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-12-07 20:18:57 +0000

    replace module.exports with export default

--- packages/core/playground/samples/additionalProperties.tsx
+++ packages/core/playground/samples/additionalProperties.tsx
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   schema: {
     title: "A customizable registration form",
     description: "A simple form with additional properties example.",
--- packages/core/playground/samples/date.tsx
+++ packages/core/playground/samples/date.tsx
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   schema: {
     title: "Date and time widgets",
     type: "object",
