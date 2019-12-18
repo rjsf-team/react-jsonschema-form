9b1b4520c961d1e16effcb75a386f94919f33032
Olli Raitio <olli.raitio@helsinki.fi>
2019-12-18 12:41:51 +0200

    fix Form.getStateFromProps logic when noValidate: shouldn't use state errors

--- packages/core/src/components/Form.js
+++ packages/core/src/components/Form.js
@@ -65,13 +65,11 @@ export default class Form extends Component {
     const retrievedSchema = retrieveSchema(schema, definitions, formData);
     const customFormats = props.customFormats;
     const additionalMetaSchemas = props.additionalMetaSchemas;
-    const validationHandledExternally =
-      !props.noValidate && props.extraErrors === state.extraErrors;
     let { errors, errorSchema } = mustValidate
       ? this.validate(formData, schema, additionalMetaSchemas, customFormats)
       : {
-          errors: (validationHandledExternally && state.errors) || [],
-          errorSchema: (validationHandledExternally && state.errorSchema) || {},
+          errors: (!props.noValidate && state.errors) || [],
+          errorSchema: (!props.noValidate && state.errorSchema) || {},
         };
     if (props.extraErrors) {
       errorSchema = mergeObjects(errorSchema, props.extraErrors);
