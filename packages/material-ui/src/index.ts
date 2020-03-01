0ae5380429b7c6333f4b970688fdba43c8d3bd1d
Kiswendsida Kikone <kikonejacob@gmail.com>
2020-03-01 07:22:11 -0600

    export ArrayFieldTemplate from material UI theme

--- packages/material-ui/src/index.ts
+++ packages/material-ui/src/index.ts
@@ -4,6 +4,7 @@ export { default as Fields } from './Fields';
 export { default as FieldTemplate } from './FieldTemplate';
 export { default as MuiForm } from './MuiForm';
 export { default as ObjectFieldTemplate } from './ObjectFieldTemplate';
+export { default as ArrayFieldTemplate } from './ArrayFieldTemplate';
 export { default as Theme } from './Theme';
 export { default as Widgets } from './Widgets';
 
