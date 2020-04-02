13245e63c43701996ee724a823b23cdf71099330
Zachary Lovelady <108858+zlove@users.noreply.github.com>
2020-04-02 11:12:42 -0700

    Setting tagName in Semantic Theme, so form is wrapped correctly

--- packages/semantic-ui/src/Theme/Theme.js
+++ packages/semantic-ui/src/Theme/Theme.js
@@ -1,20 +1,23 @@
-import { getDefaultRegistry } from 'react-jsonschema-form/lib/utils';
-import ArrayFieldTemplate from '../ArrayFieldTemplate';
-import ErrorList from '../ErrorList';
-import Fields from '../Fields';
-import FieldTemplate from '../FieldTemplate';
-import ObjectFieldTemplate from '../ObjectFieldTemplate';
-import Widgets from '../Widgets';
+import { getDefaultRegistry } from "react-jsonschema-form/lib/utils";
+import { Form as SuiForm } from "semantic-ui-react";
+
+import ArrayFieldTemplate from "../ArrayFieldTemplate";
+import ErrorList from "../ErrorList";
+import Fields from "../Fields";
+import FieldTemplate from "../FieldTemplate";
+import ObjectFieldTemplate from "../ObjectFieldTemplate";
+import Widgets from "../Widgets";
 
 const { fields, widgets } = getDefaultRegistry();
 
 const Theme = {
   ArrayFieldTemplate,
+  ErrorList,
   fields: { ...fields, ...Fields },
   FieldTemplate,
   ObjectFieldTemplate,
+  tagName: SuiForm,
   widgets: { ...widgets, ...Widgets },
-  ErrorList,
 };
 
 export default Theme;
