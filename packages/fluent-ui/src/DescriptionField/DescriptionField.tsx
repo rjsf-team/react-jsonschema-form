07c4434d37f49a8d35303f9d096852747d85cee9
Siva Ramaswami <sramaswami11@gmail.com>
2020-05-19 13:51:51 -0400

    Removed commented out lines (#6)

--- packages/fluent-ui/src/DescriptionField/DescriptionField.tsx
+++ packages/fluent-ui/src/DescriptionField/DescriptionField.tsx
@@ -2,27 +2,11 @@ import React from "react";
 
 import { FieldProps } from "@rjsf/core";
 
-//import { makeStyles } from "@material-ui/styles";
-//import Typography from '@material-ui/core/Typography';
-//import { Text, ITextProps } from "office-ui-fabric-react/lib/Text";
 import { Text } from "office-ui-fabric-react/lib/Text";
-
-/*const useStyles = makeStyles({
-  root: {
-    marginTop: 5,
-  },
-});*/
 
 const DescriptionField = ({ description }: FieldProps) => {
   if (description) {
-    //const classes = useStyles();
-
-    return (
-      /*<Typography variant="subtitle2" className={classes.root}>
-        {description}
-      </Typography>*/
-      <Text>{description}</Text>
-    );
+    return <Text>{description}</Text>;
   }
 
   return null;
