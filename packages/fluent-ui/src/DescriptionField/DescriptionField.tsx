ddfcc5a723b54be8260d45fb773af547115a5aad
Siva Ramaswami <sramaswami11@gmail.com>
2020-05-19 10:38:30 -0400

    Add TitleField and DescriptionField (#3)
    * converted TitleField to use the label field from fluentui
    
    * Converted DescriptionField to Text in FluentUI
    
    * fix tests, update snapshots
    
    Co-authored-by: Ashwin Ramaswami <aramaswamis@gmail.com>

--- packages/fluent-ui/src/DescriptionField/DescriptionField.tsx
+++ packages/fluent-ui/src/DescriptionField/DescriptionField.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import { FieldProps } from '@rjsf/core';
+import { FieldProps } from "@rjsf/core";
 
-import { makeStyles } from '@material-ui/styles';
-import Typography from '@material-ui/core/Typography';
+//import { makeStyles } from "@material-ui/styles";
+//import Typography from '@material-ui/core/Typography';
+//import { Text, ITextProps } from "office-ui-fabric-react/lib/Text";
+import { Text } from "office-ui-fabric-react/lib/Text";
 
-const useStyles = makeStyles({
+/*const useStyles = makeStyles({
   root: {
     marginTop: 5,
   },
-});
+});*/
 
 const DescriptionField = ({ description }: FieldProps) => {
   if (description) {
-    const classes = useStyles();
+    //const classes = useStyles();
 
     return (
-      <Typography variant="subtitle2" className={classes.root}>
+      /*<Typography variant="subtitle2" className={classes.root}>
         {description}
-      </Typography>
+      </Typography>*/
+      <Text>{description}</Text>
     );
   }
 
