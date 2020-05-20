537779c971d6118a30caa06c530f3b92eea6a7b0
Siva Ramaswami <sramaswami11@gmail.com>
2020-05-19 21:46:07 -0400

    Increased the font size of the title (#8)

--- packages/fluent-ui/src/TitleField/TitleField.tsx
+++ packages/fluent-ui/src/TitleField/TitleField.tsx
@@ -3,10 +3,19 @@ import React from "react";
 import { FieldProps } from "@rjsf/core";
 
 import { Label } from "office-ui-fabric-react/lib/Label";
+//import color from "@material-ui/core/colors/yellow";
+
+const styles = {
+  root: [
+    {
+      fontSize: 24,
+    },
+  ],
+};
 
 const TitleField = ({ title }: FieldProps) => (
   <>
-    <Label>{title}</Label>
+    <Label styles={styles}>{title}</Label>
   </>
 );
 
