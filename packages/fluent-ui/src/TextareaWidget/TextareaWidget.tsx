47f2b53efd58b679a22af9c98ad037c34cd58b2e
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-03 08:43:12 -0400

    fix textareawidget

--- packages/fluent-ui/src/TextareaWidget/TextareaWidget.tsx
+++ packages/fluent-ui/src/TextareaWidget/TextareaWidget.tsx
@@ -2,58 +2,21 @@ import React from "react";
 
 import { WidgetProps } from "@rjsf/core";
 
-import { TextField } from "@fluentui/react";
+import TextWidget from 'TextWidget';
 
-type CustomWidgetProps = WidgetProps & {
-  options: any;
-};
-
-const TextareaWidget = ({
-  id,
-  placeholder,
-  value,
-  required,
-  disabled,
-  autofocus,
-  label,
-  readonly,
-  onBlur,
-  onFocus,
-  onChange,
-  options,
-  schema,
-  rawErrors,
-}: CustomWidgetProps) => {
-  const _onChange = ({
-    target: { value },
-  }: React.ChangeEvent<HTMLInputElement>) =>
-    onChange(value === "" ? options.emptyValue : value);
-
-  const _onBlur = ({ target: { value } }: React.FocusEvent<HTMLInputElement>) =>
-    onBlur(id, value);
-  const _onFocus = ({
-    target: { value },
-  }: React.FocusEvent<HTMLInputElement>) => onFocus(id, value);
-
+const TextareaWidget = (props: WidgetProps) => {
+  const uiProps: any = props.options["props"] || {};
+  let options = {
+    ...props.options,
+      "ui:props": {
+      multiline: true,
+      ...uiProps
+    }
+  };
+  // TODO: rows and columns.
   return (
-    <>
-      <TextField
-        id={id}
-        label={label || schema.title}
-        placeholder={placeholder}
-        disabled={disabled || readonly}
-        value={value}
-        required={required}
-        autoFocus={autofocus}
-        multiline={true}
-        rows={options.rows || 5}
-        onChange={_onChange as any}
-        onBlur={_onBlur}
-        onFocus={_onFocus}
-        errorMessage={(rawErrors || []).join("\n")}
-      />
-    </>
+    <TextWidget {...props} options={options} />
   );
-};
+}
 
 export default TextareaWidget;
