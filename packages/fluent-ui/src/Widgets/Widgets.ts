23c9253494d22bf857f1daf4107d3a8a3643bf66
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-07 11:16:26 -0400

    add datetime / date / altdate / altdatetime widgets

--- packages/fluent-ui/src/Widgets/Widgets.ts
+++ packages/fluent-ui/src/Widgets/Widgets.ts
@@ -1,5 +1,9 @@
+import AltDateWidget from '../AltDateWidget/AltDateWidget';
+import AltDateTimeWidget from '../AltDateTimeWidget/AltDateTimeWidget';
 import CheckboxWidget from '../CheckboxWidget/CheckboxWidget';
 import CheckboxesWidget from '../CheckboxesWidget/CheckboxesWidget';
+import DateWidget from '../DateWidget/DateWidget';
+import DateTimeWidget from '../DateTimeWidget/DateTimeWidget';
 import PasswordWidget from '../PasswordWidget/PasswordWidget';
 import RadioWidget from '../RadioWidget/RadioWidget';
 import RangeWidget from '../RangeWidget/RangeWidget';
@@ -9,8 +13,12 @@ import TextWidget from '../TextWidget/TextWidget';
 import UpDownWidget from '../UpDownWidget/UpDownWidget';
 
 export default {
+  AltDateWidget,
+  AltDateTimeWidget,
   CheckboxWidget,
   CheckboxesWidget,
+  DateWidget,
+  DateTimeWidget,
   PasswordWidget,
   RadioWidget,
   RangeWidget,
