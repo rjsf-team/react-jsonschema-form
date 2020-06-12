b7691c59cf384bed47445cb461deb2974ce384dc
Siva Ramaswami <sramaswami11@gmail.com>
2020-06-12 12:33:43 -0400

    Url widget (#36)
    * URLWidget ported to Fluent UI theme
    
    * Porting URLWidget to Fluent UI theme

--- packages/fluent-ui/src/Widgets/Widgets.ts
+++ packages/fluent-ui/src/Widgets/Widgets.ts
@@ -12,6 +12,7 @@ import SelectWidget from "../SelectWidget/SelectWidget";
 import TextareaWidget from "../TextareaWidget/TextareaWidget";
 import TextWidget from "../TextWidget/TextWidget";
 import UpDownWidget from "../UpDownWidget/UpDownWidget";
+import URLWidget from "../URLWidget/URLWidget";
 
 export default {
   AltDateWidget,
@@ -28,4 +29,5 @@ export default {
   TextareaWidget,
   TextWidget,
   UpDownWidget,
+  URLWidget,
 };
