a41fae7919af2bc70a8dd638754b7137d1b7f2f0
Siva Ramaswami <sramaswami11@gmail.com>
2020-06-14 18:32:12 -0400

    Ported Emailwidget to Fluent UI theme (#37)

--- packages/fluent-ui/src/Widgets/Widgets.ts
+++ packages/fluent-ui/src/Widgets/Widgets.ts
@@ -5,6 +5,7 @@ import CheckboxesWidget from "../CheckboxesWidget/CheckboxesWidget";
 import ColorWidget from "../ColorWidget/ColorWidget";
 import DateWidget from "../DateWidget/DateWidget";
 import DateTimeWidget from "../DateTimeWidget/DateTimeWidget";
+import EmailWidget from "../EmailWidget/EmailWidget";
 import PasswordWidget from "../PasswordWidget/PasswordWidget";
 import RadioWidget from "../RadioWidget/RadioWidget";
 import RangeWidget from "../RangeWidget/RangeWidget";
@@ -22,6 +23,7 @@ export default {
   ColorWidget,
   DateWidget,
   DateTimeWidget,
+  EmailWidget,
   PasswordWidget,
   RadioWidget,
   RangeWidget,
