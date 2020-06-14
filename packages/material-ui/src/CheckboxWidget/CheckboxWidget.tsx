c50e1bd8b176c3f8251ead4c065298f81224cf08
Stan Lemon <stanlemon@users.noreply.github.com>
2020-06-14 12:39:38 -0400

    Better consistency with Material UI patterns with usage of <TextField /> (#1790)
    This PR brings better consistency with Material UI patterns. It attempts to use < TextField /> from Material UI whenever possible. There is an extra <FormControl /> component in the current component tree and this fixes that (see images for before/after of <TextWidget />). It also moves the required property up to the main <FieldTemplate /> and then sets it on the specific <TextField /> components as well. This change makes the error behavior more consistent with the way <TextField /> usually works out of the box. I also made minor adjustments to the <List /> and <ListItem /> used for errors to make the UI more consistent with what you see at https://material-ui.com/components/text-fields/#validation. Finally it adjusts widgets like <SelectWidget /> to use <TextField /> rather than reconstruct that component.

--- packages/material-ui/src/CheckboxWidget/CheckboxWidget.tsx
+++ packages/material-ui/src/CheckboxWidget/CheckboxWidget.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-import { WidgetProps } from '@rjsf/core';
+import { WidgetProps } from "@rjsf/core";
 
 const CheckboxWidget = (props: WidgetProps) => {
   const {
@@ -29,23 +28,21 @@ const CheckboxWidget = (props: WidgetProps) => {
   }: React.FocusEvent<HTMLButtonElement>) => onFocus(id, value);
 
   return (
-    <FormControl fullWidth={true} required={required}>
-      <FormControlLabel
-        control={
-          <Checkbox
-            id={id}
-            checked={typeof value === 'undefined' ? false : value}
-            required={required}
-            disabled={disabled || readonly}
-            autoFocus={autofocus}
-            onChange={_onChange}
-            onBlur={_onBlur}
-            onFocus={_onFocus}
-          />
-        }
-        label={label}
-      />
-    </FormControl>
+    <FormControlLabel
+      control={
+        <Checkbox
+          id={id}
+          checked={typeof value === "undefined" ? false : value}
+          required={required}
+          disabled={disabled || readonly}
+          autoFocus={autofocus}
+          onChange={_onChange}
+          onBlur={_onBlur}
+          onFocus={_onFocus}
+        />
+      }
+      label={label}
+    />
   );
 };
 
