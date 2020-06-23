93d6f9b1beb9867a5a2b66d94114e3178a9ebab1
Anix <anik220798@gmail.com>
2020-06-23 10:33:13 +0000

    chore: upate the tests and added mock handlers

--- packages/bootstrap-4/src/ColorWidget/ColorWidget.tsx
+++ packages/bootstrap-4/src/ColorWidget/ColorWidget.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import TextWidget, { TextWidgetProps } from "../TextWidget";
 
-export interface ColorWidget extends TextWidgetProps {
-    type?: string;
-}
-
 const ColorWidget = (props: TextWidgetProps) => {
   return <TextWidget type="color" {...props} />;
 };
--- packages/bootstrap-4/test/colorWidget.test.tsx
+++ packages/bootstrap-4/test/colorWidget.test.tsx
@@ -1,37 +1,12 @@
 import React from "react";
 import ColorWidget from "../src/ColorWidget";
-import { JSONSchema7 } from "json-schema";
 import renderer from "react-test-renderer";
+import { makeWidgetMockProps } from "./helpers/createMocks";
 
 describe("ColorWidget", () => {
   test("simple", () => {
-    const schema: JSONSchema7 = {
-      type: "array",
-      items: {
-        type: "string",
-      },
-    };
-    const mockHandler = (): void => void 0;
     const tree = renderer
-      .create(
-        <ColorWidget
-          schema={schema}
-          required
-          disabled={false}
-          readonly
-          autofocus
-          label="Some simple label"
-          onChange={mockHandler}
-          onBlur={mockHandler}
-          onFocus={mockHandler}
-          multiple={false}
-          rawErrors={[""]}
-          value="value"
-          options={{}}
-          formContext={{}}
-          id="_id"
-        />
-      )
+      .create(<ColorWidget {...makeWidgetMockProps({})} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
