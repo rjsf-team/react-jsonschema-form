d310bf43cfffc4a40e0dd8ae01979b81001815de
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-25 15:24:42 -0400

    Add Chromium flags

--- packages/playground/jest-puppeteer.config.js
+++ packages/playground/jest-puppeteer.config.js
@@ -1,8 +1,15 @@
-module.exports = {
-  server: {
-    // In CI mode, run from built production version instead of the dev version.
-    command: "serve -l 8080",
-    port: 8080,
-    usedPortAction: "kill"
-  }
+const getConfig = require('jest-puppeteer-docker/lib/config');
+
+const baseConfig = getConfig();
+const customConfig = Object.assign({}, baseConfig);
+
+customConfig.server = {
+  // In CI mode, run from built production version instead of the dev version.
+  command: "serve -l 8080",
+  port: 8080,
+  usedPortAction: "kill"
 };
+
+customConfig.chromiumFlags = ['--font-render-hinting=none', '--enable-font-antialiasing'];
+
+module.exports = customConfig;
