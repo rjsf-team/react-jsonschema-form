2d404a30e1e5ab411c1918dc042e43f15b6e7911
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-25 15:11:33 -0400

    Update snapshots, turn on live validation

--- packages/playground/test/index.test.js
+++ packages/playground/test/index.test.js
@@ -1,5 +1,5 @@
 const { toMatchImageSnapshot } = require("jest-image-snapshot");
-const { sampleNames } =  require("../src/samples");
+const { sampleNames } = require("../src/samples");
 const { themeNames } = require("../src/themes");
 expect.extend({ toMatchImageSnapshot });
 
@@ -9,20 +9,24 @@ describe("test", () => {
   beforeAll(async () => {
     await page.goto(url);
     await page.setViewport({ width: 1280, height: 1280 });
+    // Turn on live validation
+    const checkbox = await page.$$("#rjsf_options_validate");
+    await checkbox[0].click();
   });
 
   it("full page", async () => {
     const image = await page.screenshot({ fullPage: true });
     expect(image).toMatchImageSnapshot();
   });
-  
+
   async function sampleTest(themeName, sampleName) {
     const tab = await page.$x(`//a[contains(text(), '${sampleName}')]`);
+    await tab[0].click();
     await tab[0].click();
     await page.select("#rjsf_themeSelector", themeName);
     const frame = await page.$("iframe");
     const image = await page.screenshot({
-      clip: await frame.boundingBox()
+      clip: await frame.boundingBox(),
     });
     expect(image).toMatchImageSnapshot();
   }
@@ -35,4 +39,3 @@ describe("test", () => {
     }
   }
 });
-
