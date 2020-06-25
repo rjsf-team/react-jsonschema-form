64c8eb78a63ec9a4d47efc0b836d684f01f605ec
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-06-25 15:28:33 -0400

    fix puppeteer config

--- packages/playground/jest-puppeteer.config.js
+++ packages/playground/jest-puppeteer.config.js
@@ -1,15 +1,9 @@
-const getConfig = require('jest-puppeteer-docker/lib/config');
-
-const baseConfig = getConfig();
-const customConfig = Object.assign({}, baseConfig);
-
-customConfig.server = {
-  // In CI mode, run from built production version instead of the dev version.
-  command: "serve -l 8080",
-  port: 8080,
-  usedPortAction: "kill"
+module.exports = {
+  server: {
+    // In CI mode, run from built production version instead of the dev version.
+    command: "serve -l 8080",
+    port: 8080,
+    usedPortAction: "kill"
+  },
+  chromiumFlags: ['--font-render-hinting=none', '--enable-font-antialiasing']
 };
-
-customConfig.chromiumFlags = ['--font-render-hinting=none', '--enable-font-antialiasing'];
-
-module.exports = customConfig;
