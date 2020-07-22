ea7555243fb96777ae16063640c9c0efd391a9d4
Anix <anik220798@gmail.com>
2020-07-22 12:45:16 +0000

    chore: deleted extra files

--- packages/bootstrap-4/src/MuiForm/MuiForm.tsx
+++
@@ -1,8 +0,0 @@
-import { withTheme, FormProps } from '@rjsf/core';
-
-import Theme from '../Theme';
-import { StatelessComponent } from 'react';
-
-const MuiForm: React.ComponentClass<FormProps<any>> | StatelessComponent<FormProps<any>>  = withTheme(Theme);
-
-export default MuiForm;
