5ca41154e1c590e8a4eb0fb7e7affd54027773f0
Jeongbin Park <pjb7687@gmail.com>
2020-12-20 19:12:51 +0100

    Update ObjectFieldTemplate.tsx

--- packages/bootstrap-4/src/ObjectFieldTemplate/ObjectFieldTemplate.tsx
+++ packages/bootstrap-4/src/ObjectFieldTemplate/ObjectFieldTemplate.tsx
@@ -33,7 +33,7 @@ const ObjectFieldTemplate = ({
       )}
       <Container fluid className="p-0">
         {properties.map((element: any, index: number) => (
-          <Row key={index} style={{ marginBottom: "10px" }}>
+          <Row key={index} style={{ marginBottom: "10px" }} className={(element.content.uiSchema["ui:widget"] === "hidden")?"d-none":undefined} >
             <Col xs={12}> {element.content}</Col>
           </Row>
         ))}
