7d0c5c1aaee7e1025fcc71a55499214da9efb965
Enno Thoma <ennothoma@Air-von-Enno.fritz.box>
2021-05-02 11:44:45 +0200

    Add testing for antd theme

--- packages/antd/test/Array.test.js
+++ packages/antd/test/Array.test.js
@@ -22,6 +22,25 @@ describe("array fields", () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
+  test("array with custom addButtonText", () => {
+    const schema = {
+      type: "array",
+      items: {
+        type: "string"
+      }
+    };
+    const uiSchema = {
+      "ui:addButtonText": "Custom button text"
+    };
+    const tree = renderer
+        .create(<Form schema={schema} uiSchema={uiSchema} />)
+        .toJSON();
+
+    const buttonNode = tree.children[0].children[0].children[0].children[1].children[0]
+        .children[0].children[0].children[1];
+
+    expect(buttonNode.children[0]).toBe(" Custom button text");
+  });
   test("fixed array", () => {
     const schema = {
       type: "array",
