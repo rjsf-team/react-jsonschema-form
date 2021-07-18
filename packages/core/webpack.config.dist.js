b4364196d5b89ddb528aa1b6ad3470f0aaee7736
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2021-07-18 14:05:55 -0400

    2474 drop unused monaco editor output from core build

--- packages/core/webpack.config.dist.js
+++ packages/core/webpack.config.dist.js
@@ -1,6 +1,5 @@
 var path = require("path");
 var webpack = require("webpack");
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
   mode: "production",
@@ -15,9 +14,6 @@ module.exports = {
     libraryTarget: "umd"
   },
   plugins: [
-    new MonacoWebpackPlugin({
-      languages: ['json']
-    }),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("production")
