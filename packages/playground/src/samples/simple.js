782ac4d42cdf7497fa59d507f647d4684df0430c
Joris W <joris@jor.is>
2021-08-09 14:14:13 +0200

    Simple example: Sync schema, uiSchema and formData
    It makes no sense to have properties commented out in form schema, or to have uiSchema entries for which no schema properties exist. This example was a mess, while it's the first example your first time users will ever see.

--- packages/playground/src/samples/simple.js
+++ packages/playground/src/samples/simple.js
@@ -14,19 +14,19 @@ module.exports = {
         type: "string",
         title: "Last name",
       },
-      // age: {
-      //   type: "integer",
-      //   title: "Age",
-      // },
-      // bio: {
-      //   type: "string",
-      //   title: "Bio",
-      // },
-      // password: {
-      //   type: "string",
-      //   title: "Password",
-      //   minLength: 3,
-      // },
+      age: {
+        type: "integer",
+        title: "Age",
+      },
+      bio: {
+        type: "string",
+        title: "Bio",
+      },
+      password: {
+        type: "string",
+        title: "Password",
+        minLength: 3,
+      },
       telephone: {
         type: "string",
         title: "Telephone",
@@ -47,7 +47,7 @@ module.exports = {
     age: {
       "ui:widget": "updown",
       "ui:title": "Age of person",
-      "ui:description": "(earthian year)",
+      "ui:description": "(earth year)",
     },
     bio: {
       "ui:widget": "textarea",
@@ -55,9 +55,6 @@ module.exports = {
     password: {
       "ui:widget": "password",
       "ui:help": "Hint: Make it strong!",
-    },
-    date: {
-      "ui:widget": "alt-datetime",
     },
     telephone: {
       "ui:options": {
@@ -70,5 +67,6 @@ module.exports = {
     age: 75,
     bio: "Roundhouse kicking asses since 1940",
     password: "noneed",
+    telephone: "1-800-KICKASS"
   },
 };
