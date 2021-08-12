73ac83c32d3a8c4c795786598e63a9dbd2b4feb3
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-08-12 08:38:03 -0400

    Update packages/semantic-ui/src/TextWidget/TextWidget.js

--- packages/semantic-ui/src/TextWidget/TextWidget.js
+++ packages/semantic-ui/src/TextWidget/TextWidget.js
@@ -3,7 +3,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Form } from "semantic-ui-react";
 import { getSemanticProps } from "../util";
-import {  utils } from "@rjsf/core";
+import { utils } from "@rjsf/core";
 const { getDisplayLabel } = utils;
 function TextWidget(props) {
   const {
