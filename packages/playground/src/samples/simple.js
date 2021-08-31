3cc9efb839978a488f2f1986d8ad29fd2c9bb16a
Joris W <joris@jor.is>
2021-08-31 15:13:57 +0200

    Run Prettier

--- packages/playground/src/samples/simple.js
+++ packages/playground/src/samples/simple.js
@@ -67,6 +67,6 @@ module.exports = {
     age: 75,
     bio: "Roundhouse kicking asses since 1940",
     password: "noneed",
-    telephone: "1-800-KICKASS"
+    telephone: "1-800-KICKASS",
   },
 };
