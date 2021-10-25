c4f377b4a8c6d36078456fad52b616cd871aaa7c
EssiSupponen <essi.supponen@visma.com>
2021-10-25 10:33:01 +0300

    KEHFORM-436: Add min and max labels, add labels when there are 10 or less options

--- packages/material-ui/src/RangeWidget/RangeWidget.tsx
+++ packages/material-ui/src/RangeWidget/RangeWidget.tsx
@@ -8,6 +8,30 @@ import { WidgetProps } from "@visma/rjsf-core";
 
 const { rangeSpec } = utils;
 
+const generateWithMiddleMarks = (min: number, max: number, step: number) => {
+  const middleMarks = [];
+
+  for (let i = min; i < max; i = i + step) {
+    middleMarks.push({value: i, label: i.toString()});
+  }
+
+  middleMarks.push({value: max, label: max.toString()})
+
+  return middleMarks;
+}
+
+const generateMarks = (min?: number, max?: number, step?: number) => {
+  if (min === undefined || max === undefined ) {
+    return [];
+  }
+
+  if ((max-min)/step! < 10) {
+    return generateWithMiddleMarks(min!, max!, step!);
+  }
+
+  return [{value: min!, label: min!.toString()}, {value: max!, label: max!.toString()}];
+}
+
 const RangeWidget = ({
   value,
   readonly,
@@ -31,6 +55,8 @@ const RangeWidget = ({
     target: { value },
   }: React.FocusEvent<HTMLInputElement>) => onFocus(id, value);
 
+  const marks = generateMarks(sliderProps.min, sliderProps.max, sliderProps.step);
+
   return (
     <>
       <FormLabel required={required} id={id}>
@@ -42,6 +68,7 @@ const RangeWidget = ({
         onBlur={_onBlur}
         onFocus={_onFocus}
         valueLabelDisplay="auto"
+        marks={marks}
         {...sliderProps}
       />
     </>
