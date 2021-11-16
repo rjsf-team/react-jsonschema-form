541fffdffac18228101c9adfeed9211897970b6d
Heath Chiavettone <heath.chiavettone@freenome.com>
2021-11-15 23:56:15 -0800

    Made the Themes be tolerant of not having the library actually present

--- packages/material-ui/src/Theme/Theme.tsx
+++ packages/material-ui/src/Theme/Theme.tsx
@@ -1,88 +1,98 @@
 import React, { PropsWithChildren, Ref } from 'react';
-import {
-  Box,
-  Button,
-  Checkbox,
-  Divider,
-  FormControl,
-  FormControlLabel,
-  FormHelperText,
-  FormGroup,
-  FormLabel,
-  Grid,
-  IconButton,
-  Input,
-  InputLabel,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  MenuItem,
-  Paper,
-  Radio,
-  RadioGroup,
-  TextField,
-  Typography,
-} from '@material-ui/core'
-import {
-  Add as AddIcon,
-  ArrowUpward as ArrowUpwardIcon,
-  ArrowDownward as ArrowDownwardIcon,
-  Error as ErrorIcon,
-  Remove as RemoveIcon,
-} from '@material-ui/icons';
 import { ThemeProps } from '@rjsf/core';
 
 import MuiComponentContext from '../MuiComponentContext';
+import MaterialUIContextProps from 'Theme/MaterialUIContextProps';
 import ThemeCommon from '../ThemeCommon';
 
-const DefaultChildren = () => (
-  <Box marginTop={3}>
-    <Button type="submit" variant="contained" color="primary">
-      Submit
-    </Button>
-  </Box>
-);
+const mui = require('@material-ui/core');
+const icons = require('@material-ui/icons');
 
-export const Mui4Context = {
-  Button,
-  Box,
-  Checkbox,
-  Divider,
-  Grid,
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  FormHelperText,
-  FormLabel,
-  IconButton,
-  Input,
-  InputLabel,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  MenuItem,
-  Paper,
-  Radio,
-  RadioGroup,
-  TextField,
-  Typography,
-  AddIcon,
-  ArrowDownwardIcon,
-  ArrowUpwardIcon,
-  ErrorIcon,
-  RemoveIcon,
-};
+export let Mui4Context: MaterialUIContextProps;
+let DefaultChildren = () => <div>@material-ui not available</div>;
+
+if (mui && Object.keys(mui).length && icons && Object.keys(icons).length) {
+  const {
+    Box,
+    Button,
+    Checkbox,
+    Divider,
+    FormControl,
+    FormControlLabel,
+    FormHelperText,
+    FormGroup,
+    FormLabel,
+    Grid,
+    IconButton,
+    Input,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    MenuItem,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography,
+  } = mui;
+  const {
+    Add: AddIcon,
+    ArrowUpward: ArrowUpwardIcon,
+    ArrowDownward: ArrowDownwardIcon,
+    Error: ErrorIcon,
+    Remove: RemoveIcon,
+  } = icons;
+
+  DefaultChildren = () => (
+    <Box marginTop={3}>
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+    </Box>
+  );
+
+  Mui4Context = {
+    Button,
+    Box,
+    Checkbox,
+    Divider,
+    Grid,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormHelperText,
+    FormLabel,
+    IconButton,
+    Input,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    MenuItem,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography,
+    AddIcon,
+    ArrowDownwardIcon,
+    ArrowUpwardIcon,
+    ErrorIcon,
+    RemoveIcon,
+  };
+}
 
 const Mui4TagName = React.forwardRef(
   function Mui4TagName(props: Omit<PropsWithChildren<HTMLFormElement>, 'contentEditable'>,
                        ref: Ref<HTMLFormElement>) {
     const { children, ...rest } = props;
     return (
-      <MuiComponentContext.Provider value={Mui4Context}>
+      <MuiComponentContext.Provider value={Mui4Context || {}}>
         <form ref={ref} {...rest}>
-          {children}
+          {Mui4Context ? children : <div>@material/core and/or @material/icons is not available</div>}
         </form>
       </MuiComponentContext.Provider>
     );
--- packages/material-ui/src/Theme5/Theme5.tsx
+++ packages/material-ui/src/Theme5/Theme5.tsx
@@ -1,88 +1,98 @@
 import React, { PropsWithChildren, Ref } from 'react';
-import {
-  Box,
-  Button,
-  Checkbox,
-  Divider,
-  FormControl,
-  FormControlLabel,
-  FormHelperText,
-  FormGroup,
-  FormLabel,
-  Grid,
-  IconButton,
-  InputLabel,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  MenuItem,
-  OutlinedInput,
-  Paper,
-  Radio,
-  RadioGroup,
-  TextField,
-  Typography,
-} from '@mui/material'
-import {
-  Add as AddIcon,
-  ArrowUpward as ArrowUpwardIcon,
-  ArrowDownward as ArrowDownwardIcon,
-  Error as ErrorIcon,
-  Remove as RemoveIcon,
-} from '@mui/icons-material';
 import { ThemeProps } from '@rjsf/core';
 
 import MuiComponentContext from '../MuiComponentContext';
+import Mui5ContextProps from 'Theme5/Mui5ContextProps';
 import ThemeCommon from '../ThemeCommon';
 
-const DefaultChildren5 = () => (
-  <Box marginTop={3}>
-    <Button type="submit" variant="contained" color="primary">
-      Submit
-    </Button>
-  </Box>
-);
+const mui = require('@mui/material');
+const icons = require('@mui/icons-material');
 
-export const Mui5Context = {
-  Button,
-  Box,
-  Checkbox,
-  Divider,
-  Grid,
-  FormControl,
-  FormControlLabel,
-  FormGroup,
-  FormHelperText,
-  FormLabel,
-  IconButton,
-  Input: OutlinedInput,
-  InputLabel,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  MenuItem,
-  Paper,
-  Radio,
-  RadioGroup,
-  TextField,
-  Typography,
-  AddIcon,
-  ArrowDownwardIcon,
-  ArrowUpwardIcon,
-  ErrorIcon,
-  RemoveIcon,
-};
+export let Mui5Context: Mui5ContextProps;
+let DefaultChildren = () => <div>@mui not available</div>;
+
+if (mui && Object.keys(mui).length && icons && Object.keys(icons).length) {
+  const {
+    Box,
+    Button,
+    Checkbox,
+    Divider,
+    FormControl,
+    FormControlLabel,
+    FormHelperText,
+    FormGroup,
+    FormLabel,
+    Grid,
+    IconButton,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    MenuItem,
+    OutlinedInput,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography,
+  } = mui;
+  const {
+    Add: AddIcon,
+    ArrowUpward: ArrowUpwardIcon,
+    ArrowDownward: ArrowDownwardIcon,
+    Error: ErrorIcon,
+    Remove: RemoveIcon,
+  } = icons;
+
+  DefaultChildren = () => (
+    <Box marginTop={3}>
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+    </Box>
+  );
+
+  Mui5Context = {
+    Button,
+    Box,
+    Checkbox,
+    Divider,
+    Grid,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormHelperText,
+    FormLabel,
+    IconButton,
+    Input: OutlinedInput,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    MenuItem,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography,
+    AddIcon,
+    ArrowDownwardIcon,
+    ArrowUpwardIcon,
+    ErrorIcon,
+    RemoveIcon,
+  };
+}
 
 const Mui5TagName = React.forwardRef(
   function Mui5TagName(props: Omit<PropsWithChildren<HTMLFormElement>, 'contentEditable'>,
                        ref: Ref<HTMLFormElement>) {
     const { children, ...rest } = props;
     return (
-      <MuiComponentContext.Provider value={Mui5Context}>
+      <MuiComponentContext.Provider value={Mui5Context || {}}>
         <form ref={ref} {...rest}>
-          {children}
+          {Mui5Context ? children : <div>@mui/material and/or @mui/icons-material is not available</div>}
         </form>
       </MuiComponentContext.Provider>
     );
@@ -91,7 +101,7 @@ const Mui5TagName = React.forwardRef(
 
 const Theme5: ThemeProps = {
   tagName: Mui5TagName,
-  children: <DefaultChildren5 />,
+  children: <DefaultChildren />,
   ...ThemeCommon,
 };
 
