a8414a6aa918840a347ce9f9c7e0efb99c1aced2
Rodrigo Fuentes <rod.fuentes@users.noreply.github.com>
2022-01-14 17:09:06 -0500

    adds filter for props

--- packages/chakra-ui/src/utils.ts
+++ packages/chakra-ui/src/utils.ts
@@ -1,4 +1,4 @@
-import { ChakraProps } from "@chakra-ui/react";
+import { ChakraProps, shouldForwardProp } from "@chakra-ui/react";
 import { FormProps, UiSchema } from "@rjsf/core";
 
 export type ThemeProps<T = any> = Omit<FormProps<T>, "schema" | "uiSchema"> & {
@@ -16,6 +16,22 @@ interface GetChakraProps {
 }
 
 export function getChakra({ uiSchema }: GetChakraProps): ChakraProps {
-  const chakraProps = uiSchema["ui:options"] && uiSchema["ui:options"].chakra;
-  return Object.assign({}, chakraProps) as ChakraProps;
+  let chakraProps =
+    (uiSchema["ui:options"] && uiSchema["ui:options"].chakra) ?? {};
+
+  Object.keys(chakraProps).forEach(key => {
+    /**
+     * Leveraging `shouldForwardProp` to remove props
+     *
+     * This is a utility function that's used in `@chakra-ui/react`'s factory function.
+     * Normally, it prevents ChakraProps from being passed to the DOM.
+     * In this case we just want to delete the unknown props. So we flip the boolean.
+     */
+    if (shouldForwardProp(key)) {
+      // @ts-ignore - How to type this?!... 😬
+      delete chakraProps[key];
+    }
+  });
+
+  return chakraProps as ChakraProps;
 }
