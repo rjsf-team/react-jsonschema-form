769a03506b38c9176caafb5706cf3ff4e3459797
Jacques <jacqueswho@gmail.com>
2022-03-15 20:09:12 +0200

    fix: include correct code again for submit button props

--- packages/chakra-ui/src/SubmitButton/SubmitButton.tsx
+++ packages/chakra-ui/src/SubmitButton/SubmitButton.tsx
@@ -5,8 +5,8 @@ import { utils,WidgetProps } from "@rjsf/core";
 const {getSubmitButtonOptions} = utils;
 const SubmitButton = ({ uiSchema }: WidgetProps) => {
 
-  const { submitText, removed, props: submitButtonProps }= getSubmitButtonOptions(uiSchema);
-  if (removed) {return null;}
+  const { submitText, norender, props: submitButtonProps }= getSubmitButtonOptions(uiSchema);
+  if (norender) {return null;}
 
   return (
     <Box marginTop={3}>
