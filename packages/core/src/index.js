b133f8b13ac711e451c28e31a01d0a4e7b645afb
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-04-09 21:54:43 +0000

    Build all packages with tsdx

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,6 +1,12 @@
 import Form from "./components/Form";
 import withTheme from "./withTheme";
-import * as utils from "./utils";
+import * as _utils from "./utils";
+import { getDefaultRegistry } from "./defaultRegistry";
+
+const utils = {
+  ..._utils,
+  getDefaultRegistry,
+};
 
 export { withTheme, utils };
 export default Form;
