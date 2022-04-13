86a722c9e4549510d2ca0879676eb30d4a7fbe53
Jacques Nel <jacqueswho@gmail.com>
2022-04-13 21:10:38 +0200

    feat: wrap additional properties

--- packages/semantic-ui/src/FieldTemplate/FieldTemplate.js
+++ packages/semantic-ui/src/FieldTemplate/FieldTemplate.js
@@ -4,7 +4,8 @@ import { Form } from "semantic-ui-react";
 import DescriptionField from "../DescriptionField";
 import HelpField from "../HelpField";
 import RawErrors from "../RawErrors";
-import { getSemanticProps, getSemanticErrorProps, MaybeWrap } from "../util";
+import WrapIfAdditional from './WrapIfAdditional';
+import { getSemanticProps,getSemanticErrorProps, MaybeWrap } from "../util";
 
 function FieldTemplate({
   id,
@@ -15,28 +16,43 @@ function FieldTemplate({
   label,
   rawErrors = [],
   rawHelp,
+  hidden,
   rawDescription,
   ...props
 }) {
   const semanticProps = getSemanticProps(props);
   const { wrapLabel, wrapContent } = semanticProps;
   const errorOptions = getSemanticErrorProps(props);
-  return (
-    <Form.Group key={id} widths="equal" grouped>
-      <MaybeWrap wrap={wrapContent} className="sui-field-content">
-        {children}
-        {displayLabel && rawDescription && (
-          <MaybeWrap wrap={wrapLabel} className="sui-field-label">
-            {rawDescription && (
-              <DescriptionField description={rawDescription} />
-            )}
-          </MaybeWrap>
-        )}
-        <HelpField helpText={rawHelp} id={id + "__help"} />
-        <RawErrors errors={rawErrors} options={errorOptions} />
-      </MaybeWrap>
-    </Form.Group>
-  );
+  
+  if (hidden) {
+    return children;
+  }
+
+  return (<WrapIfAdditional
+      classNames={classNames}
+      id={id}
+      label={label}
+      {...props}
+    >
+      {id === 'root' ? (
+        children
+      ) : (
+      <Form.Group key={id} widths="equal" grouped>
+        <MaybeWrap wrap={wrapContent} className="sui-field-content">
+          {children}
+          {displayLabel && rawDescription && (
+            <MaybeWrap wrap={wrapLabel} className="sui-field-label">
+              {rawDescription && (
+                <DescriptionField description={rawDescription} />
+              )}
+            </MaybeWrap>
+          )}
+          <HelpField helpText={rawHelp} id={id + "__help"} />
+          <RawErrors errors={rawErrors} options={errorOptions} />
+        </MaybeWrap>
+      </Form.Group>)
+    }
+  </WrapIfAdditional>);
 }
 
 export default FieldTemplate;
