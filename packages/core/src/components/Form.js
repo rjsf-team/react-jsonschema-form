3f9088e99835a4d7f23e7341f10fc8b38997a18f
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-04-14 21:22:19 +0000

    Move getDefaultRegistry to prevent dependency cycle

--- packages/core/src/components/Form.js
+++ packages/core/src/components/Form.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import _pick from "lodash/pick";
 import _get from "lodash/get";
 import _isEmpty from "lodash/isEmpty";
+import { getDefaultRegistry } from "../defaultRegistry";
 
 import { default as DefaultErrorList } from "./ErrorList";
 import {
@@ -10,7 +11,6 @@ import {
   retrieveSchema,
   shouldRender,
   toIdSchema,
-  getDefaultRegistry,
   deepEquals,
   toPathSchema,
   isObject,
