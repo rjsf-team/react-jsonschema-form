2f513e7e892320141c39d23e8bee4768931d8b64
Heath Chiavettone <heath.chiavettone@freenome.com>
2022-05-31 09:42:10 -0700

    - Responded to self-review comments

--- packages/utils/src/toConstant.ts
+++ packages/utils/src/toConstant.ts
@@ -4,9 +4,9 @@ import { RJSFSchema } from './types';
 export default function toConstant(schema: RJSFSchema) {
   if (Array.isArray(schema.enum) && schema.enum.length === 1) {
     return schema.enum[0];
-  } else if (schema.hasOwnProperty(CONST_NAME)) {
-    return schema.const;
-  } else {
-    throw new Error('schema cannot be inferred as a constant');
   }
+  if (schema.hasOwnProperty(CONST_NAME)) {
+    return schema.const;
+  }
+  throw new Error('schema cannot be inferred as a constant');
 }
