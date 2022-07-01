188e038a03e17e7d20869770711aa6ceaf03b4ad
Heath C <51679588+heath-freenome@users.noreply.github.com>
2022-07-01 11:59:16 -0700

    Validator ajv6 (#2891)
    * Added new package for validator-ajv6
    - Refactored `packages/core/src/validate.js` into this new package
    
    * - Switched to the new `@rjsf/utils` library and types
    
    * - Finished types conversion to @rjsf/utils
    - Began adding tests
    
    * - Completed conversion for validator class-based tests. Just need to get to 100%
    1
    
    * - Finished 100% test coverage and documentation of the `AJV6Validator`
    
    * - Completed the documentation of the remaining methods and types for the library
    
    * - Responded to self-review feedback and also fixed tests to hopefully get it to pass
    
    * - Fixed the build (this time hopefully)
    
    * - Switched to just passing `stack` instead of the whole empty object
    
    * - Added hacky run of tests from within the utils directory
    
    * - Fixed a few little things
    
    * - Fixed bug in custom validation, including adding a test to detect it
    
    * - Removed schema utils test for `stubExistingAdditionalPropertiesTest()` as it no longer exists
    
    * - Fixed self-review feedback and the import in the `test/createAjvInstance.test.ts`
    - Also bumped all the packages that made sense

---
+++ packages/validator-ajv6/test/utilsTests/schema.test.ts
@@ -0,0 +1,25 @@
+// With Lerna active, the test world has access to the test suite via the symlink
+import {
+  getDefaultFormStateTest,
+  getDisplayLabelTest,
+  getMatchingOptionTest,
+  isFilesArrayTest,
+  isMultiSelectTest,
+  isSelectTest,
+  retrieveSchemaTest,
+  toIdSchemaTest,
+  toPathSchemaTest
+} from '@rjsf/utils/test/schema';
+import getTestValidator from './getTestValidator';
+
+const testValidator = getTestValidator({});
+
+getDefaultFormStateTest(testValidator);
+getDisplayLabelTest(testValidator);
+getMatchingOptionTest(testValidator);
+isFilesArrayTest(testValidator);
+isMultiSelectTest(testValidator);
+isSelectTest(testValidator);
+retrieveSchemaTest(testValidator);
+toIdSchemaTest(testValidator);
+toPathSchemaTest(testValidator);
