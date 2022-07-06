3f6ed1d9074e00a323f05bb49b5e01777de58bcb
Heath Chiavettone <heath.chiavettone@freenome.com>
2022-07-06 11:56:58 -0700

    - Export `WithThemeProps` from core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,8 @@
 import Form, { FormProps, FormState } from './components/Form';
-import withTheme from './withTheme';
+import withTheme, { WithThemeProps } from './withTheme';
 import getDefaultRegistry from './getDefaultRegistry';
 
-export type { FormProps, FormState };
+export type { FormProps, FormState, WithThemeProps };
 
 export { withTheme, getDefaultRegistry };
 export default Form;
