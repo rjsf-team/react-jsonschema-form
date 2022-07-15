5804bbbf485f4ee07559e1ac263c2e8ab4a5e37d
Heath C <51679588+heath-freenome@users.noreply.github.com>
2022-07-15 10:44:27 -0700

    Added ajvOptionsOverrides to support user-provided AJV options overriding (#2929)
    - Updated `CustomValidatorOptionsType` to add `ajvOptionsOverrides`
    - Updated the `createAjvInstance()` function to spread any `ajvOptionsOverrides` on top of the `AJV_CONFIG`
    - Updated `AJV6Validator` constructor to pass `ajvOptionsOverrides` to `createAjvInstance()`
    - Updated tests to add test-case for the `$data` flag mentioned in #1668

--- packages/validator-ajv6/src/types.ts
+++ packages/validator-ajv6/src/types.ts
@@ -1,3 +1,5 @@
+import { Options } from 'ajv';
+
 /** The type describing how to customize the AJV6 validator
  */
 export interface CustomValidatorOptionsType {
@@ -5,4 +7,6 @@ export interface CustomValidatorOptionsType {
   additionalMetaSchemas?: ReadonlyArray<object>;
   /** The set of additional custom formats that the validator will support */
   customFormats?: { [k: string]: string | RegExp | ((data: string) => boolean) };
+  /** The set of config overrides that will be passed to the AJV validator constructor on top of the defaults */
+  ajvOptionsOverrides?: Options;
 }
