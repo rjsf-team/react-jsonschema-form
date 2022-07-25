d9aaf3d79051823a659ad47428ba1368f3b660ba
Jakob Klepp <jakob.klepp@moonvision.io>
2022-07-25 16:17:11 +0200

    Example without imports

--- packages/playground/src/samples/customFieldAnyOf.js
+++ packages/playground/src/samples/customFieldAnyOf.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ObjectField from "@rjsf/core/lib/components/fields/ObjectField";
 
 export default {
   schema: {
@@ -30,22 +29,78 @@ export default {
     ],
   },
   uiSchema: {
-    "ui:field": ({ schema, ...other }) => (
-      <div style={{ display: "flex" }}>
-        {schema.anyOf.map((subSchema, i) => (
-          <div
-            key={`subSchema ${i}`}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "50%",
-              margin: "1rem",
-            }}>
-            <ObjectField {...other} schema={subSchema} />
+    "ui:field": ({ schema, idSchema: {$id}, formData, onChange, ...other }) => {
+      console.log("ui:field", { schema, ...other });
+      const changeHandlerFactory = fieldName => event => {
+        console.log("changeHandler", fieldName, {event});
+        onChange(
+          formData
+            ? {...formData, [fieldName]: event.target.value}
+            : {[fieldName]: event.target.value}
+        );
+      }
+      return (
+        <>
+          <h4>Location</h4>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "50%",
+                margin: "1rem",
+              }}>
+              <div className="form-group field field-string">
+                <label className="control-label" htmlFor={`${$id}-city`}>
+                  City
+                </label>
+                <input
+                  className="form-control"
+                  id={`${$id}-city`}
+                  required=""
+                  placeholder=""
+                  type="text"
+                  value={formData?.city || ""}
+                  onChange={changeHandlerFactory("city")}
+                />
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "50%",
+                margin: "1rem",
+              }}>
+              <div className="form-group field field-string">
+                <label className="control-label" htmlFor={`${$id}-lat`}>
+                  Latitude
+                </label>
+                <input
+                  className="form-control"
+                  id={`${$id}-lat`}
+                  type="number"
+                  value={formData?.lat || 0}
+                  onChange={changeHandlerFactory("lat")}
+                />
+              </div>
+              <div className="form-group field field-string">
+                <label className="control-label" htmlFor={`${$id}-lon`}>
+                  Longitude
+                </label>
+                <input
+                  className="form-control"
+                  id={`${$id}-lon`}
+                  type="number"
+                  value={formData?.lon || 0}
+                  onChange={changeHandlerFactory("lon")}
+                />
+              </div>
+            </div>
           </div>
-        ))}
-      </div>
-    ),
+        </>
+      );
+    },
   },
   formData: {},
 };
