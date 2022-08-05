01af026de04996c4399adb8d7019fef2b6b579ce
Heath Chiavettone <heath.chiavettone@freenome.com>
2022-08-05 13:42:13 -0700

    Implement BaseInputTemplate in material-ui - Refactored `TextWidget` as `BaseInputTemplate`, using the `getInputProps()` to implement `core` feature - Deleted the following widgets because they are now just duplicate code to what is implemented with the template in `core`   - `ColorWidget`, `EmailWidget`, `PasswordWidget`, `URLWidget`, `UpDownWidget`   - Updated the `widgets` overrides in `Widgets.tsx` to remove the deleted class references - Updated `CheckboxesWidget`, `RadioWidget` to add missing `id` associated with the `htmlFor` - Updated `DateTimeWidget` `DateWidget` and `TextareaWidget` to use the `BaseInputTemplate` - Updated the test snapshots to match the subtle little changes caused by the `getInputProps()` feature and increased use of the `BaseInputTemplate`   - Also deleted some tests and snapshots that were for now deleted widgets

--- packages/material-ui/src/TextareaWidget/TextareaWidget.tsx
+++ packages/material-ui/src/TextareaWidget/TextareaWidget.tsx
@@ -1,55 +1,16 @@
 import React from "react";
-import TextField from "@material-ui/core/TextField";
 import { WidgetProps } from "@rjsf/utils";
 
-const TextareaWidget = ({
-  id,
-  placeholder,
-  value,
-  required,
-  disabled,
-  autofocus,
-  label,
-  readonly,
-  onBlur,
-  onFocus,
-  onChange,
-  options,
-  schema,
-  rawErrors = [],
-}: WidgetProps) => {
-  const _onChange = ({
-    target: { value },
-  }: React.ChangeEvent<HTMLInputElement>) =>
-    onChange(value === "" ? options.emptyValue : value);
-  const _onBlur = ({ target: { value } }: React.FocusEvent<HTMLInputElement>) =>
-    onBlur(id, value);
-  const _onFocus = ({
-    target: { value },
-  }: React.FocusEvent<HTMLInputElement>) => onFocus(id, value);
+const TextareaWidget = (props: WidgetProps) => {
+  const { options, registry } = props;
+  const { BaseInputTemplate } = registry.templates;
 
   let rows: string | number = 5;
   if (typeof options.rows === "string" || typeof options.rows === "number") {
     rows = options.rows;
   }
 
-  return (
-    <TextField
-      id={id}
-      label={label || schema.title}
-      placeholder={placeholder}
-      disabled={disabled || readonly}
-      value={value}
-      required={required}
-      autoFocus={autofocus}
-      multiline={true}
-      rows={rows}
-      error={rawErrors.length > 0}
-      onChange={_onChange}
-      onBlur={_onBlur}
-      onFocus={_onFocus}
-    />
-  );
+  return <BaseInputTemplate {...props} multiline rows={rows} />;
 };
 
 export default TextareaWidget;
