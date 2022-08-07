c8f941df23723bd4b22124c2517a91349f4b5072
Heath C <51679588+heath-freenome@users.noreply.github.com>
2022-08-07 00:55:00 -0700

    Implement BaseInputTemplate in material-ui (#2993)
    - Refactored `TextWidget` as `BaseInputTemplate`, using the `getInputProps()` to implement `core` feature
    - Deleted the following widgets because they are now just duplicate code to what is implemented with the template in `core`
      - `ColorWidget`, `EmailWidget`, `PasswordWidget`, `URLWidget`, `UpDownWidget`
      - Updated the `widgets` overrides in `Widgets.tsx` to remove the deleted class references
    - Updated `CheckboxesWidget`, `RadioWidget` to add missing `id` associated with the `htmlFor`
    - Updated `DateTimeWidget` `DateWidget` and `TextareaWidget` to use the `BaseInputTemplate`
    - Updated the test snapshots to match the subtle little changes caused by the `getInputProps()` feature and increased use of the `BaseInputTemplate`
      - Also deleted some tests and snapshots that were for now deleted widgets

--- packages/material-ui/src/DateTimeWidget/DateTimeWidget.tsx
+++ packages/material-ui/src/DateTimeWidget/DateTimeWidget.tsx
@@ -3,14 +3,14 @@ import { localToUTC, utcToLocal, WidgetProps } from "@rjsf/utils";
 
 const DateTimeWidget = (props: WidgetProps) => {
   const { registry } = props;
-  const { TextWidget } = registry.widgets;
+  const { BaseInputTemplate } = registry.templates;
   const value = utcToLocal(props.value);
   const onChange = (value: any) => {
     props.onChange(localToUTC(value));
   };
 
   return (
-    <TextWidget
+    <BaseInputTemplate
       type="datetime-local"
       InputLabelProps={{
         shrink: true,
