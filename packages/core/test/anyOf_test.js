bc5d036c70a04f5fbd1373d7c40bd474946b593b
Jakob Klepp <1183303+truh@users.noreply.github.com>
2022-08-19 17:13:37 +0200

    fix(@rjsf/core): custom field that is any of displays twice (#2890)
    * Create playground example *Any Of with Custom Field*
    
    * Fix Bug: Custom field that is anyOf displays twice
    
    https://github.com/rjsf-team/react-jsonschema-form/issues/2691
    
    * Update CHANGELOG.md
    
    * Avoid breaking anyOf/oneOf select with ObjectField
    
    * Verify custom field is only rendered once with anyOf/oneOf
    
    * Verify select is not rendered when custom field set
    
    * Example without imports
    
    * Cleanup
    
    * Fix merge  conflict
    
    * Delete SchemaField.js
    
    * Update changelog
    
    * Run cs-format

--- packages/core/test/anyOf_test.js
+++ packages/core/test/anyOf_test.js
@@ -1173,4 +1173,30 @@ describe("anyOf", () => {
       expect(inputs).to.have.length.of(0);
     });
   });
+
+  describe("Custom Field", function () {
+    const schema = {
+      anyOf: [
+        {
+          type: "number",
+        },
+        {
+          type: "string",
+        },
+      ],
+    };
+    const uiSchema = {
+      "ui:field": () => <div className="custom-field">Custom field</div>,
+    };
+    it("should be rendered once", function () {
+      const { node } = createFormComponent({ schema, uiSchema });
+      const fields = node.querySelectorAll(".custom-field");
+      expect(fields).to.have.length.of(1);
+    });
+    it("should not render <select>", function () {
+      const { node } = createFormComponent({ schema, uiSchema });
+      const selects = node.querySelectorAll("select");
+      expect(selects).to.have.length.of(0);
+    });
+  });
 });
