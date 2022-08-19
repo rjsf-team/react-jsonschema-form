71bd7b234ceb837750b9db79b95101fe72b350da
Heath C <51679588+heath-freenome@users.noreply.github.com>
2022-08-19 10:47:28 -0700

    Fixed Form and withTheme to properly merge ButtonTemplates (#3027)
    * Fixed Form and withTheme to properly merge ButtonTemplates
    - Updated `Form` to update the `templates` prop to make `ButtonTemplates` optional (via `Omit<>` and explicit add)
      - Updated the `getRegistry()` function to merge `ButtonTemplates` from `props.templates.ButtonProps` and the default `templates.ButtonProps`
    - Updated the `withTheme()` function to merge `ButtonTemplates` from the theme and user props
      - Added tests to ensure the merge is happening
    - Updated all the themes no longer pull the `getDefaultRegistry()` in as the registry merge happens properly in the two core components
    
    * - Responded to self-review

--- packages/chakra-ui/src/Theme/Theme.tsx
+++ packages/chakra-ui/src/Theme/Theme.tsx
@@ -1,14 +1,11 @@
-import { WithThemeProps, getDefaultRegistry } from "@rjsf/core";
+import { WithThemeProps } from "@rjsf/core";
 
 import Templates from "../Templates";
 import Widgets from "../Widgets";
 
-const { fields, templates, widgets } = getDefaultRegistry();
-
 const Theme: WithThemeProps = {
-  fields: fields,
-  templates: { ...templates, ...Templates },
-  widgets: { ...widgets, ...Widgets },
+  templates: Templates,
+  widgets: Widgets,
 };
 
 export default Theme;
