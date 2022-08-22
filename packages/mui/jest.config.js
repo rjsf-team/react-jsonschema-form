1d79bee8f3d33e5eefef8fa3cc29ca5cef270683
Heath C <51679588+heath-freenome@users.noreply.github.com>
2022-08-21 17:43:49 -0700

    Convert NullField and NumberField to functional components (#3030)
    - Added or updated `jest.config.js` files to all themes that add `moduleNameMapper` for `react` and `react-dom` to avoid hooks issues
    - Updated `NullField` and `NumberField` to be stateless functional components

--- packages/mui/jest.config.js
+++ packages/mui/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  moduleNameMapper: {
+    "^react$": "<rootDir>/node_modules/react",
+    "^react-dom$": "<rootDir>/node_modules/react-dom",
+  },
   snapshotSerializers: ["@emotion/jest/serializer"],
   testEnvironment: "jsdom",
   testEnvironmentOptions: {
