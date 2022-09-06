3959f9bf31860356a7d539a0ad70852b8cd8bf56
Heath Chiavettone <heath.chiavettone@freenome.com>
2022-09-06 16:29:08 -0700

    Add ajv 8 validator to core tests and to playground w/ l12n - Updated `core` to add `@rjsf/validator-ajv8` as a devDependency and replicated the ajv6 tests for ajv8 with message text changes - Updated `playground` to add `@rjsf/validator-ajv8` and `ajv-i18n` to support changing to validators   - Added the default `AJV8` validator and a `AJV8_es` (using `ajv-i18n`) validator to the validators list - Updated the `README.md` for `utils` to make it describe utils rather than the ajv6 validator - Updated descriptions of tests in `validator-ajv6` to properly describe some test cases - Updated `validator-ajv8` to add support for a localizer as follows:   - Added a new `Localizer` type to for the localizer function   - Updated the `customizeValidator()` function to take an optional second `localizer` parameter which it passes into the `AJV8Validator` constructor   - Updated the `AJV8Validator` class to add an internal `localizer: Localizer` variable that is initialized from the new, optional `localizer` parameter on the constructor   - Updated the `validateFormData()` function to call the `this.localizer()` with the `this.ajv.errors` if a function is provided   - Updated the tests for the `customizeValidator()` and `AJV8Validator` to test the new behavior - Updated the `validation.md` documentation for this new feature - Updated the `CHANGELOG.md` for these changes

--- packages/validator-ajv8/src/customizeValidator.ts
+++ packages/validator-ajv8/src/customizeValidator.ts
@@ -1,15 +1,17 @@
 import { ValidatorType } from "@rjsf/utils";
 
-import { CustomValidatorOptionsType } from "./types";
+import { CustomValidatorOptionsType, Localizer } from "./types";
 import AJV8Validator from "./validator";
 
 /** Creates and returns a customized implementation of the `ValidatorType` with the given customization `options` if
  * provided.
  *
  * @param [options={}] - The `CustomValidatorOptionsType` options that are used to create the `ValidatorType` instance
+ * @param [localizer] - If provided, is used to localize a list of Ajv `ErrorObject`s
  */
 export default function customizeValidator<T = any>(
-  options: CustomValidatorOptionsType = {}
+  options: CustomValidatorOptionsType = {},
+  localizer?: Localizer
 ): ValidatorType<T> {
-  return new AJV8Validator<T>(options);
+  return new AJV8Validator<T>(options, localizer);
 }
