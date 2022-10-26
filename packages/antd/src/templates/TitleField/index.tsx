cc0ccae392afa788257e6eca3cd78a525d4bdf21
Heath C <51679588+heath-freenome@users.noreply.github.com>
2022-10-26 09:29:36 -0700

    Fix: bump dependencies (#3209)
    Fix #3198 by bumping the dependencies that do not break things
    - Updated all of the dependencies that do not break things in all the packages
    - In `antd` the minor upgrade cause a few typing fixing as well as regenerating the snapshots with the a change caused by the `antd` library fix
    - In `semantic-ui` also added a `.npmrc` file to allow `legacy peer dependencies`

--- packages/antd/src/templates/TitleField/index.tsx
+++ packages/antd/src/templates/TitleField/index.tsx
@@ -4,17 +4,21 @@ import { TitleFieldProps } from "@rjsf/utils";
 import { withConfigConsumer } from "antd/lib/config-provider/context";
 
 // Add in the `prefixCls` element needed by the `withConfigConsumer` HOC
-export type AntdTitleFieldProps = TitleFieldProps & { prefixCls: string };
+export type AntdTitleFieldProps = TitleFieldProps & {
+  prefixCls: string;
+  formContext: object;
+};
 
 const TitleField = ({
   id,
   prefixCls,
   required,
   registry,
+  formContext: formContext1,
   title,
 }: AntdTitleFieldProps) => {
   const { formContext } = registry;
-  const { colon = true } = formContext;
+  const { colon = true } = { ...formContext1, ...formContext };
 
   let labelChildren = title;
   if (colon && typeof title === "string" && title.trim() !== "") {
