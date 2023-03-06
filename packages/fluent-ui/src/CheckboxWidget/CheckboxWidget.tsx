ceb66e63998dac9806df18300e752f4c529a5475
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2023-03-06 17:33:15 +0330

    fixed React imports

--- packages/fluent-ui/src/CheckboxWidget/CheckboxWidget.tsx
+++ packages/fluent-ui/src/CheckboxWidget/CheckboxWidget.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { Checkbox } from "@fluentui/react";
 import {
   ariaDescribedByIds,
@@ -52,7 +53,7 @@ export default function CheckboxWidget<
     options,
   } = props;
 
-  const _onChange = React.useCallback(
+  const _onChange = useCallback(
     (_, checked?: boolean): void => {
       onChange(checked);
     },
