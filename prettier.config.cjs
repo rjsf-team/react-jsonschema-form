6611a44bc031ff36e78d1c0b5df4ddf788e7eb6e
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2023-03-06 22:38:59 +0330

    changed prettier config to use types

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  bracketSameLine: false,
+  trailingComma: "es5",
+  useTabs: false,
+  semi: true,
+  tabWidth: 2,
+};
+
+module.exports = config;
