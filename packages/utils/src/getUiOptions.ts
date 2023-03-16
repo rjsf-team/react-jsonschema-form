06063dd1eff435cc66577b1c92db2bb7b01d764b
Heath Chiavettone <heath.chiavettone@freenome.com>
2023-03-16 09:39:20 -0700

    - Fixed format issue

--- packages/utils/src/getUiOptions.ts
+++ packages/utils/src/getUiOptions.ts
@@ -15,15 +15,18 @@ export default function getUiOptions<T = any, S extends StrictRJSFSchema = RJSFS
 ): UIOptionsType<T, S, F> {
   return Object.keys(uiSchema)
     .filter((key) => key.indexOf('ui:') === 0)
-    .reduce((options, key) => {
-      const value = uiSchema[key];
-      if (key === UI_WIDGET_KEY && isObject(value)) {
-        console.error('Setting options via ui:widget object is no longer supported, use ui:options instead');
-        return options;
-      }
-      if (key === UI_OPTIONS_KEY && isObject(value)) {
-        return { ...options, ...value };
-      }
-      return { ...options, [key.substring(3)]: value };
-    }, { ...globalOptions });
+    .reduce(
+      (options, key) => {
+        const value = uiSchema[key];
+        if (key === UI_WIDGET_KEY && isObject(value)) {
+          console.error('Setting options via ui:widget object is no longer supported, use ui:options instead');
+          return options;
+        }
+        if (key === UI_OPTIONS_KEY && isObject(value)) {
+          return { ...options, ...value };
+        }
+        return { ...options, [key.substring(3)]: value };
+      },
+      { ...globalOptions }
+    );
 }
