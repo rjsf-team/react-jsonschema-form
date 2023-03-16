aa5f1619b149ca07c3b177af9cba4d47247008c3
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2023-03-16 22:06:35 +0330

    added Editor callback deps

--- packages/playground/src/components/Editor.tsx
+++ packages/playground/src/components/Editor.tsx
@@ -13,19 +13,22 @@ const Editor: React.FC<{ title: string; code: string; onChange: (code: string) =
     const [valid, setValid] = useState(true);
     const [code, setCode] = useState(initialCode);
 
-    const onCodeChange = useCallback((code: string | undefined) => {
-      if (!code) return;
+    const onCodeChange = useCallback(
+      (code: string | undefined) => {
+        if (!code) return;
 
-      try {
-        const parsedCode = JSON.parse(code);
-        setValid(true);
-        setCode(code);
-        onChange(parsedCode);
-      } catch (err) {
-        setValid(false);
-        setCode(code);
-      }
-    }, []);
+        try {
+          const parsedCode = JSON.parse(code);
+          setValid(true);
+          setCode(code);
+          onChange(parsedCode);
+        } catch (err) {
+          setValid(false);
+          setCode(code);
+        }
+      },
+      [setValid, setCode, onChange]
+    );
 
     const icon = valid ? 'ok' : 'remove';
     const cls = valid ? 'valid' : 'invalid';
