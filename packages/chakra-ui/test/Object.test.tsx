ce8004b5d74a0725133230a2b35f3112cdbeb60f
Heath C <51679588+heath-freenome@users.noreply.github.com>
2023-03-27 10:31:32 -0700

    chore: Centralize the snapshot tests for objects under core (#3544)
    To minimize the duplication of snapshot testing code across themes, centralized the object ones in core in a manner similar to how schema tests work with utils and the validators.

--- packages/chakra-ui/test/Object.test.tsx
+++ packages/chakra-ui/test/Object.test.tsx
@@ -1,29 +1,5 @@
-/** @jsx jsx */
+import objectTests from '@rjsf/core/testSnap/objectTests';
 
-import { RJSFSchema } from '@rjsf/utils';
-import validator from '@rjsf/validator-ajv8';
-import renderer from 'react-test-renderer';
+import Form from '../src';
 
-import Form from '../src/index';
-
-describe('object fields', () => {
-  test('object', () => {
-    const schema: RJSFSchema = {
-      type: 'object',
-      properties: {
-        a: { type: 'string', title: 'A' },
-        b: { type: 'number', title: 'B' },
-      },
-    };
-    const tree = renderer.create(<Form schema={schema} validator={validator} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  test('additionalProperties', () => {
-    const schema: RJSFSchema = {
-      type: 'object',
-      additionalProperties: true,
-    };
-    const tree = renderer.create(<Form schema={schema} validator={validator} formData={{ foo: 'foo' }} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
+objectTests(Form);
--- packages/semantic-ui/test/Object.test.tsx
+++ packages/semantic-ui/test/Object.test.tsx
@@ -1,6 +1,4 @@
-import { RJSFSchema } from '@rjsf/utils';
-import validator from '@rjsf/validator-ajv8';
-import renderer from 'react-test-renderer';
+import objectTests from '@rjsf/core/testSnap/objectTests';
 
 import Form from '../src';
 
@@ -10,24 +8,4 @@ jest.mock('@fluentui/react-component-ref', () => ({
   Ref: jest.fn().mockImplementation(({ children }) => children),
 }));
 
-describe('object fields', () => {
-  test('object', () => {
-    const schema: RJSFSchema = {
-      type: 'object',
-      properties: {
-        a: { type: 'string', title: 'A' },
-        b: { type: 'number', title: 'B' },
-      },
-    };
-    const tree = renderer.create(<Form schema={schema} validator={validator} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-  test('additionalProperties', () => {
-    const schema: RJSFSchema = {
-      type: 'object',
-      additionalProperties: true,
-    };
-    const tree = renderer.create(<Form schema={schema} validator={validator} formData={{ foo: 'foo' }} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
+objectTests(Form);
