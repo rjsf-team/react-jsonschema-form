c83a8fb5898568f2565ed46be0fdc6eb9e07ceb9
Heath Chiavettone <heath.chiavettone@freenome.com>
2023-03-29 17:13:54 -0700

    chore: Update Objects tests to tests all the label variations   Updated the `objectTests` to add testing of titles and descriptions from both schema and uiSchema, along with disabling labels globally - Updated the snapshots

--- packages/core/testSnap/objectTests.tsx
+++ packages/core/testSnap/objectTests.tsx
@@ -1,13 +1,38 @@
 import { ComponentType } from 'react';
 import renderer from 'react-test-renderer';
-import { RJSFSchema } from '@rjsf/utils';
+import { RJSFSchema, UiSchema } from '@rjsf/utils';
 import validator from '@rjsf/validator-ajv8';
 
 import { FormProps } from '../src';
 
-// export type ObjectRenderCustomOptions = {
-//  checkboxes?: TestRendererOptions;
-// }
+const titleAndDesc = {
+  title: 'Test field',
+  description: 'a test description'
+};
+
+const uiTitleAndDesc: UiSchema = {
+  'ui:options': {
+    title: 'My Field',
+    description: 'a fancier description'
+  },
+  a: {
+    'ui:options': {
+      title: 'My Item A',
+      description: 'a fancier item A description'
+    }
+  },
+  b: {
+    'ui:options': {
+      title: 'My Item B',
+      description: 'a fancier item B description'
+    }
+  }
+
+};
+
+const labelsOff: UiSchema = {
+  'ui:globalOptions': { label: false }
+};
 
 export default function arrayTests(Form: ComponentType<FormProps>) {
   describe('object fields', () => {
@@ -29,6 +54,96 @@ export default function arrayTests(Form: ComponentType<FormProps>) {
       };
       const tree = renderer.create(<Form schema={schema} validator={validator} formData={{ foo: 'foo' }} />).toJSON();
       expect(tree).toMatchSnapshot();
+    });
+    describe('with title and description', () => {
+      test('object', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          ...titleAndDesc,
+          properties: {
+            a: { type: 'string', title: 'A', description: 'A description' },
+            b: { type: 'number', title: 'B', description: 'B description' },
+          },
+        };
+        const tree = renderer.create(<Form schema={schema} validator={validator} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+      test('additionalProperties', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          ...titleAndDesc,
+          additionalProperties: true,
+        };
+        const tree = renderer.create(<Form schema={schema} validator={validator} formData={{ foo: 'foo' }} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+    });
+    describe('with title and description from uiSchema', () => {
+      test('object', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          properties: {
+            a: { type: 'string', title: 'A' },
+            b: { type: 'number', title: 'B' },
+          },
+        };
+        const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+      test('additionalProperties', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          additionalProperties: true,
+        };
+        const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} formData={{ foo: 'foo' }} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+    });
+    describe('with title and description from both', () => {
+      test('object', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          ...titleAndDesc,
+          properties: {
+            a: { type: 'string', title: 'A', description: 'A description' },
+            b: { type: 'number', title: 'B', description: 'B description' },
+          },
+        };
+        const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+      test('additionalProperties', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          ...titleAndDesc,
+          additionalProperties: true,
+        };
+        const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} formData={{ foo: 'foo' }} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+    });
+    describe('with title and description with global label off', () => {
+      test('object', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          ...titleAndDesc,
+          properties: {
+            a: { type: 'string', title: 'A', description: 'A description' },
+            b: { type: 'number', title: 'B', description: 'B description' },
+          },
+        };
+        const tree = renderer.create(<Form schema={schema} uiSchema={labelsOff} validator={validator} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
+      test('additionalProperties', () => {
+        const schema: RJSFSchema = {
+          type: 'object',
+          ...titleAndDesc,
+          additionalProperties: true,
+        };
+        const tree = renderer.create(<Form schema={schema} uiSchema={labelsOff} validator={validator} formData={{ foo: 'foo' }} />).toJSON();
+        expect(tree).toMatchSnapshot();
+      });
     });
   });
 }
