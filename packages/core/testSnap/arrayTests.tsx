f3d9b7886001291424d2fa40ffb12238742119a0
Heath C <51679588+heath-freenome@users.noreply.github.com>
2023-03-30 07:52:28 -0700

    chore: Update Array tests to tests all the label variations (#3555)
    - Updated the `arrayTests` to add testing of titles and descriptions from both schema and uiSchema, along with disabling labels globally
    - Fixed a bug in `SelectWidget` id generation that caused tests to no pass due to varying id

--- packages/core/testSnap/arrayTests.tsx
+++ packages/core/testSnap/arrayTests.tsx
@@ -5,32 +5,32 @@ import validator from '@rjsf/validator-ajv8';
 
 import { FormProps } from '../src';
 
-// const titleAndDesc = {
-//   title: 'Test field',
-//   description: 'a test description'
-// };
-//
-// const itemTitleAndDesc = {
-//   title: 'Test item',
-//   description: 'a test item description'
-// };
-//
-// const uiTitleAndDesc: UiSchema = {
-//   'ui:options': {
-//     title: 'My Field',
-//     description: 'a fancier description'
-//   },
-//   items: {
-//     'ui:options': {
-//       title: 'My Item',
-//       description: 'a fancier item description'
-//     }
-//   }
-// };
-//
-// const labelsOff: UiSchema = {
-//   'ui:globalOptions': { label: false }
-// };
+const titleAndDesc = {
+  title: 'Test field',
+  description: 'a test description'
+};
+
+const itemTitleAndDesc = {
+  title: 'Test item',
+  description: 'a test item description'
+};
+
+const uiTitleAndDesc: UiSchema = {
+  'ui:options': {
+    title: 'My Field',
+    description: 'a fancier description'
+  },
+  items: {
+    'ui:options': {
+      title: 'My Item',
+      description: 'a fancier item description'
+    }
+  }
+};
+
+const labelsOff: UiSchema = {
+  'ui:globalOptions': { label: false }
+};
 
 export const CHECKBOXES_CUSTOMIZE = 'checkboxes';
 
@@ -139,253 +139,253 @@ export default function arrayTests(Form: ComponentType<FormProps>, customOptions
       expect(tree).toMatchSnapshot();
     });
   });
-  // describe('with title and description', () => {
-  //   test('array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         ...itemTitleAndDesc,
-  //         type: 'string',
-  //       },
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('fixed array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: [
-  //         {
-  //           ...itemTitleAndDesc,
-  //           type: 'string',
-  //         },
-  //         {
-  //           ...itemTitleAndDesc,
-  //           type: 'number',
-  //         },
-  //       ],
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('checkboxes', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         ...itemTitleAndDesc,
-  //         type: 'string',
-  //         enum: ['a', 'b', 'c'],
-  //       },
-  //       uniqueItems: true,
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} validator={validator} />, customOptions.checkboxes)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('array icons', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         ...itemTitleAndDesc,
-  //         type: 'string',
-  //       },
-  //     };
-  //     const uiSchema: UiSchema = {
-  //       'ui:options': { copyable: true },
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  // });
-  // describe('with title and description from uiSchema', () => {
-  //   test('array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       items: {
-  //         type: 'string',
-  //       },
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('fixed array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       items: [
-  //         {
-  //           type: 'string',
-  //         },
-  //         {
-  //           type: 'number',
-  //         },
-  //       ],
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('checkboxes', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       items: {
-  //         type: 'string',
-  //         enum: ['a', 'b', 'c'],
-  //       },
-  //       uniqueItems: true,
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />, customOptions.checkboxes)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('array icons', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       items: {
-  //         type: 'string',
-  //       },
-  //     };
-  //     const uiSchema: UiSchema = {
-  //       'ui:copyable': true,
-  //       ...uiTitleAndDesc,
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  // });
-  // describe('with title and description from both', () => {
-  //   test('array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         type: 'string',
-  //       },
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('fixed array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: [
-  //         {
-  //           type: 'string',
-  //         },
-  //         {
-  //           type: 'number',
-  //         },
-  //       ],
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('checkboxes', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         type: 'string',
-  //         enum: ['a', 'b', 'c'],
-  //       },
-  //       uniqueItems: true,
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />, customOptions.checkboxes)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('array icons', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         type: 'string',
-  //       },
-  //     };
-  //     const uiSchema: UiSchema = {
-  //       'ui:copyable': true,
-  //       ...uiTitleAndDesc,
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  // });
-  // describe('with title and description with global label off', () => {
-  //   test('array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         ...itemTitleAndDesc,
-  //         type: 'string',
-  //       },
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} uiSchema={labelsOff} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('fixed array', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: [
-  //         {
-  //           ...itemTitleAndDesc,
-  //           type: 'string',
-  //         },
-  //         {
-  //           ...itemTitleAndDesc,
-  //           type: 'number',
-  //         },
-  //       ],
-  //     };
-  //     const tree = renderer.create(<Form schema={schema} uiSchema={labelsOff} validator={validator} />).toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('checkboxes', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         ...itemTitleAndDesc,
-  //         type: 'string',
-  //         enum: ['a', 'b', 'c'],
-  //       },
-  //       uniqueItems: true,
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} uiSchema={labelsOff} validator={validator} />, customOptions.checkboxes)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  //   test('array icons', () => {
-  //     const schema: RJSFSchema = {
-  //       type: 'array',
-  //       ...titleAndDesc,
-  //       items: {
-  //         ...itemTitleAndDesc,
-  //         type: 'string',
-  //       },
-  //     };
-  //     const uiSchema: UiSchema = {
-  //       ...labelsOff,
-  //       'ui:options': { copyable: true },
-  //     };
-  //     const tree = renderer
-  //       .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
-  //       .toJSON();
-  //     expect(tree).toMatchSnapshot();
-  //   });
-  // });
+  describe('with title and description', () => {
+    test('array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          ...itemTitleAndDesc,
+          type: 'string',
+        },
+      };
+      const tree = renderer.create(<Form schema={schema} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('fixed array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: [
+          {
+            ...itemTitleAndDesc,
+            type: 'string',
+          },
+          {
+            ...itemTitleAndDesc,
+            type: 'number',
+          },
+        ],
+      };
+      const tree = renderer.create(<Form schema={schema} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('checkboxes', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          ...itemTitleAndDesc,
+          type: 'string',
+          enum: ['a', 'b', 'c'],
+        },
+        uniqueItems: true,
+      };
+      const tree = renderer
+        .create(<Form schema={schema} validator={validator} />, customOptions.checkboxes)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('array icons', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          ...itemTitleAndDesc,
+          type: 'string',
+        },
+      };
+      const uiSchema: UiSchema = {
+        'ui:options': { copyable: true },
+      };
+      const tree = renderer
+        .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('with title and description from uiSchema', () => {
+    test('array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      };
+      const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('fixed array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        items: [
+          {
+            type: 'string',
+          },
+          {
+            type: 'number',
+          },
+        ],
+      };
+      const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('checkboxes', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        items: {
+          type: 'string',
+          enum: ['a', 'b', 'c'],
+        },
+        uniqueItems: true,
+      };
+      const tree = renderer
+        .create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />, customOptions.checkboxes)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('array icons', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        items: {
+          type: 'string',
+        },
+      };
+      const uiSchema: UiSchema = {
+        'ui:copyable': true,
+        ...uiTitleAndDesc,
+      };
+      const tree = renderer
+        .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('with title and description from both', () => {
+    test('array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          type: 'string',
+        },
+      };
+      const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('fixed array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: [
+          {
+            type: 'string',
+          },
+          {
+            type: 'number',
+          },
+        ],
+      };
+      const tree = renderer.create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('checkboxes', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          type: 'string',
+          enum: ['a', 'b', 'c'],
+        },
+        uniqueItems: true,
+      };
+      const tree = renderer
+        .create(<Form schema={schema} uiSchema={uiTitleAndDesc} validator={validator} />, customOptions.checkboxes)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('array icons', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          type: 'string',
+        },
+      };
+      const uiSchema: UiSchema = {
+        'ui:copyable': true,
+        ...uiTitleAndDesc,
+      };
+      const tree = renderer
+        .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+  describe('with title and description with global label off', () => {
+    test('array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          ...itemTitleAndDesc,
+          type: 'string',
+        },
+      };
+      const tree = renderer.create(<Form schema={schema} uiSchema={labelsOff} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('fixed array', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: [
+          {
+            ...itemTitleAndDesc,
+            type: 'string',
+          },
+          {
+            ...itemTitleAndDesc,
+            type: 'number',
+          },
+        ],
+      };
+      const tree = renderer.create(<Form schema={schema} uiSchema={labelsOff} validator={validator} />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('checkboxes', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          ...itemTitleAndDesc,
+          type: 'string',
+          enum: ['a', 'b', 'c'],
+        },
+        uniqueItems: true,
+      };
+      const tree = renderer
+        .create(<Form schema={schema} uiSchema={labelsOff} validator={validator} />, customOptions.checkboxes)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+    test('array icons', () => {
+      const schema: RJSFSchema = {
+        type: 'array',
+        ...titleAndDesc,
+        items: {
+          ...itemTitleAndDesc,
+          type: 'string',
+        },
+      };
+      const uiSchema: UiSchema = {
+        ...labelsOff,
+        'ui:options': { copyable: true },
+      };
+      const tree = renderer
+        .create(<Form schema={schema} uiSchema={uiSchema} validator={validator} formData={['a', 'b']} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
 }
