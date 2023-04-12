093f340983c0762231b95ef8965206a36d8811a5
Nathanael Olander <nate.olander@crypticvector.com>
2023-04-12 16:24:07 -0400

    Correct createSchemaUtils types

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -968,14 +968,12 @@ export interface SchemaUtilsType<T = any, S extends StrictRJSFSchema = RJSFSchem
    * @param [includeUndefinedValues=false] - Optional flag, if true, cause undefined values to be added as defaults.
    *          If "excludeObjectChildren", cause undefined values for this object and pass `includeUndefinedValues` as
    *          false when computing defaults for any nested object properties.
-   * @param [behaviorBitFlags=0] Optional bitwise flags to set which behavior is chosen for certain edge cases.
    * @returns - The resulting `formData` with all the defaults provided
    */
   getDefaultFormState(
     schema: S,
     formData?: T,
-    includeUndefinedValues?: boolean | 'excludeObjectChildren',
-    behaviorBitFlags?: number
+    includeUndefinedValues?: boolean | 'excludeObjectChildren'
   ): T | T[] | undefined;
   /** Determines whether the combination of `schema` and `uiSchema` properties indicates that the label for the `schema`
    * should be displayed in a UI.
