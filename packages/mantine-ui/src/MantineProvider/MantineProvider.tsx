df7789fb8d00a8c1984c8ae253d0ac210dcbb58b
Przemyslaw Kalisiewicz <przemyslaw.kalisiewicz@gmail.com>
2023-05-12 23:17:06 +0200

    update

--- packages/mantine-ui/src/MantineProvider/MantineProvider.tsx
+++ packages/mantine-ui/src/MantineProvider/MantineProvider.tsx
@@ -1,8 +1,5 @@
-import { createEmotionCache, MantineProvider } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
-export const __createMantineFrameProvider =
-  (props: any) =>
-  ({ document }: any) => {
-    const mantineCache = createEmotionCache({ key: 'mantine', container: document.head });
-    return <MantineProvider emotionCache={mantineCache}>{props.children}</MantineProvider>;
-  };
+export const __createMantineFrameProvider = (props: any) => () => {
+  return <MantineProvider>{props.children}</MantineProvider>;
+};
