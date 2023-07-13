f2394e9dedce67b10a41eaffc561a8d90c25bd43
Bogdan Savluk <savluk.bogdan@gmail.com>
2023-07-13 18:36:51 +0200

    jest config updates

--- packages/fluent-ui/jest.config.js
+++ packages/fluent-ui/jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   moduleNameMapper: {
     'office-ui-fabric-react/lib/': 'office-ui-fabric-react/lib-commonjs/',
-    '^react$': '<rootDir>/node_modules/react',
-    '^react-dom$': '<rootDir>/node_modules/react-dom',
   },
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
