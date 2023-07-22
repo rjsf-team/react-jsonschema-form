00eb8594855fe8b1e4f3467901aaa2e9ff4caf63
Bogdan Savluk <savluk.bogdan@gmail.com>
2023-07-22 02:39:35 +0200

    fix jest environment setup

---
+++ packages/core/test/setup-jest-env.js
@@ -0,0 +1,12 @@
+import html from 'html';
+import { setImmediate } from 'timers';
+
+// atob
+global.atob = require('atob');
+
+// HTML debugging helper
+global.d = function d(node) {
+  console.log(html.prettyPrint(node.outerHTML, { indent_size: 2 }));
+};
+
+global.setImmediate = setImmediate;
