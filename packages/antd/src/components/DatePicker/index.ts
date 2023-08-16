42ec71aaa31c34f60ed71da18e394966fcf73b23
manish3323 <gowardipemanish95@gmail.com>
2023-08-16 12:57:12 +0530

    update antd version to v5 from v4

--- packages/antd/src/components/DatePicker/index.ts
+++
@@ -1,6 +0,0 @@
-import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs';
-import generatePicker from 'antd/lib/date-picker/generatePicker';
-
-const DatePicker = generatePicker(dayjsGenerateConfig);
-
-export default DatePicker;
