0a6ff5f19bff7586c4d1bb689352ab5eee3bfb5a
yuki-js <me@aoki.app>
2024-01-25 18:20:46 +0900

    fix dataURI parsing mechanism

--- packages/utils/src/dataURItoBlob.ts
+++ packages/utils/src/dataURItoBlob.ts
@@ -4,39 +4,36 @@
  * @param dataURI - The `DataUrl` potentially containing name and raw data to be converted to a Blob
  * @returns - an object containing a Blob and its name, extracted from the URI
  */
-export default function dataURItoBlob(dataURI: string) {
-  // Split metadata from data
-  const splitted: string[] = dataURI.split(',');
-  // Split params
-  const params: string[] = splitted[0].split(';');
-  // Get mime-type from params
-  const type: string = params[0].replace('data:', '');
-  // Filter the name property from params
-  const properties = params.filter((param) => {
-    return param.split('=')[0] === 'name';
-  });
-  // Look for the name and use unknown if no name property.
-  let name: string;
-  if (properties.length !== 1) {
-    name = 'unknown';
-  } else {
-    // Because we filtered out the other property,
-    // we only have the name case here, which we decode to make it human-readable
-    name = decodeURI(properties[0].split('=')[1]);
+export default function dataURItoBlob(dataURILike: string) {
+  // check if is dataURI
+  if (dataURILike.indexOf('data:') === -1) {
+    throw new Error('File is invalid: URI must be a dataURI');
   }
+  const dataURI = dataURILike.slice(5);
+  const splitted = dataURI.split(';base64,');
+  // check if the dataURI is base64
+  if (splitted.length !== 2) {
+    throw new Error('File is invalid: dataURI must be base64');
+  }
+  const [media, base64] = splitted;
+  const [mime, ...mediaparams] = media.split(';');
+  const type = mime || '';
+  const name = decodeURI(
+    mediaparams.map((param) => param.split('=')).find(([key]) => key === 'name')?.[1] || 'unknown'
+  );
 
   // Built the Uint8Array Blob parameter from the base64 string.
   try {
-    const binary = atob(splitted[1]);
-    const array = [];
+    const binary = atob(base64);
+    const array = new Array(binary.length);
     for (let i = 0; i < binary.length; i++) {
-      array.push(binary.charCodeAt(i));
+      array[i] = binary.charCodeAt(i);
     }
     // Create the blob object
     const blob = new window.Blob([new Uint8Array(array)], { type });
 
     return { blob, name };
   } catch (error) {
-    return { blob: { size: 0, type: (error as Error).message }, name: dataURI };
+    throw new Error('File is invalid: failed to decode base64');
   }
 }
--- packages/utils/test/dataURItoBlob.test.ts
+++ packages/utils/test/dataURItoBlob.test.ts
@@ -1,6 +1,45 @@
 import { dataURItoBlob } from '../src';
 
 describe('dataURItoBlob()', () => {
+  it('should pass when the data is empty', () => {
+    const { blob, name } = dataURItoBlob('data:text/plain;base64,');
+    expect(name).toEqual('unknown');
+    expect(blob).toHaveProperty('size', 0);
+    expect(blob).toHaveProperty('type', 'text/plain');
+  });
+  it('should pass when the both body and media type are empty', () => {
+    const { blob, name } = dataURItoBlob('data:;base64,');
+    expect(name).toEqual('unknown');
+    expect(blob).toHaveProperty('size', 0);
+    expect(blob).toHaveProperty('type', '');
+  });
+  it('should return the body is empty', () => {
+    const { blob, name } = dataURItoBlob('data:application/json;name=test.png;base64,');
+    expect(name).toEqual('test.png');
+    expect(blob).toHaveProperty('size', 0);
+    expect(blob).toHaveProperty('type', 'application/json');
+  });
+  it('should throw when the body is not a Base64 encoded dataURI', () => {
+    expect(() => dataURItoBlob('data:;Hello%20World')).toThrow(new Error('File is invalid: dataURI must be base64'));
+    expect(() => dataURItoBlob('data:text/plain;Hello%20World')).toThrow(
+      new Error('File is invalid: dataURI must be base64')
+    );
+    expect(() => dataURItoBlob('data:Hello%20World')).toThrow(new Error('File is invalid: dataURI must be base64'));
+  });
+  it('should throw if the body is not a valid dataURI', () => {
+    expect(() => dataURItoBlob('Hello%20World')).toThrow(new Error('File is invalid: URI must be a dataURI'));
+    expect(() => dataURItoBlob('javascript:alert()')).toThrow(new Error('File is invalid: URI must be a dataURI'));
+  });
+
+  it('should throw the body is not valid Base64', () => {
+    expect(() => dataURItoBlob('data:text/plain;base64,Hello%20World')).toThrow(
+      new Error('File is invalid: failed to decode base64')
+    );
+    expect(() => dataURItoBlob('data:text/plain;base64,こんにちわ')).toThrow(
+      new Error('File is invalid: failed to decode base64')
+    );
+  });
+
   it('should return the name of the file if present', () => {
     const { blob, name } = dataURItoBlob('data:image/png;name=test.png;base64,VGVzdC5wbmc=');
     expect(name).toEqual('test.png');
@@ -24,11 +63,5 @@ describe('dataURItoBlob()', () => {
     expect(name).toEqual('test.png');
     expect(blob).toHaveProperty('size', 8);
     expect(blob).toHaveProperty('type', 'image/png');
-  });
-  it('should return error blob when blob conversion fails', () => {
-    const { blob, name } = dataURItoBlob('test.png');
-    expect(name).toEqual('test.png');
-    expect(blob).toHaveProperty('size', 0);
-    expect(blob).toHaveProperty('type', 'The string to be decoded contains invalid characters.');
   });
 });
