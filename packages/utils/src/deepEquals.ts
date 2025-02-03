23cc1e0356308333ae5be67641de8789a3c1ff77
Heath Chiavettone <heath.chiavettone@freenome.com>
2025-02-03 09:29:45 -0800

    Revert #4446 due to regression Fixed #4475 by rolling back #4446 - In `@rjsf/utils` rolled back the changes made by #4446 - Updated the `CHANGELOG.md` accordingly

--- packages/utils/src/deepEquals.ts
+++ packages/utils/src/deepEquals.ts
@@ -1,22 +1,19 @@
-import { createCustomEqual } from 'fast-equals';
+import isEqualWith from 'lodash/isEqualWith';
 
-/** Implements a deep equals using the `fast-equals.createCustomEqual` function, providing a customized comparator that assumes all functions are equivalent.
+/** Implements a deep equals using the `lodash.isEqualWith` function, that provides a customized comparator that
+ * assumes all functions are equivalent.
  *
  * @param a - The first element to compare
  * @param b - The second element to compare
  * @returns - True if the `a` and `b` are deeply equal, false otherwise
  */
-const deepEquals = createCustomEqual({
-  createCustomConfig: () => ({
-    // Assume all functions are equivalent
-    // see https://github.com/rjsf-team/react-jsonschema-form/issues/255
-    //
-    // Performance improvement: knowing that typeof a === function, so, only needs to check if typeof b === function.
-    // https://github.com/planttheidea/fast-equals/blob/c633c4e653cacf8fd5cbb309b6841df62322d74c/src/comparator.ts#L99
-    areFunctionsEqual(_a, b) {
-      return typeof b === 'function';
-    },
-  }),
-});
-
-export default deepEquals;
+export default function deepEquals(a: any, b: any): boolean {
+  return isEqualWith(a, b, (obj: any, other: any) => {
+    if (typeof obj === 'function' && typeof other === 'function') {
+      // Assume all functions are equivalent
+      // see https://github.com/rjsf-team/react-jsonschema-form/issues/255
+      return true;
+    }
+    return undefined; // fallback to default isEquals behavior
+  });
+}
