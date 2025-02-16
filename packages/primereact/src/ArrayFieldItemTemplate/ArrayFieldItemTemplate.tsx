af3d0c4a5d16a78f1c281ce6be3b1071b17df97f
arnold <arnold@jasny.net>
2025-02-16 01:49:10 +0100

    Primereact for v6

--- packages/primereact/src/ArrayFieldItemTemplate/ArrayFieldItemTemplate.tsx
+++ packages/primereact/src/ArrayFieldItemTemplate/ArrayFieldItemTemplate.tsx
@@ -1,69 +1,31 @@
-import { ArrayFieldTemplateItemType, FormContextType, RJSFSchema, StrictRJSFSchema } from '@rjsf/utils';
+import {
+  ArrayFieldItemTemplateType,
+  FormContextType,
+  getTemplate,
+  getUiOptions,
+  RJSFSchema,
+  StrictRJSFSchema,
+} from '@rjsf/utils';
 
-/** The `ArrayFieldItemTemplate` component is the template used to render an items of an array.
- *
- * @param props - The `ArrayFieldTemplateItemType` props for the component
- */
 export default function ArrayFieldItemTemplate<
   T = any,
   S extends StrictRJSFSchema = RJSFSchema,
   F extends FormContextType = any
->(props: ArrayFieldTemplateItemType<T, S, F>) {
-  const {
-    children,
-    disabled,
-    hasToolbar,
-    hasCopy,
-    hasMoveDown,
-    hasMoveUp,
-    hasRemove,
-    index,
-    onCopyIndexClick,
-    onDropIndexClick,
-    onReorderClick,
-    readonly,
-    uiSchema,
+>(props: ArrayFieldItemTemplateType<T, S, F>) {
+  const { children, buttonsProps, hasToolbar, uiSchema, registry } = props;
+  const uiOptions = getUiOptions<T, S, F>(uiSchema);
+  const ArrayFieldItemButtonsTemplate = getTemplate<'ArrayFieldItemButtonsTemplate', T, S, F>(
+    'ArrayFieldItemButtonsTemplate',
     registry,
-  } = props;
-  const { CopyButton, MoveDownButton, MoveUpButton, RemoveButton } = registry.templates.ButtonTemplates;
+    uiOptions
+  );
   return (
     <div style={{ position: 'relative' }}>
       {hasToolbar && (
-        <div style={{ position: 'absolute', right: 0, top: '-8px' }}>
-          {(hasMoveUp || hasMoveDown || hasRemove) && (
+        <div style={{ position: 'absolute', right: 0, top: '-10px' }}>
+          {hasToolbar && (
             <div style={{ flexDirection: 'row' }}>
-              {(hasMoveUp || hasMoveDown) && (
-                <MoveUpButton
-                  disabled={disabled || readonly || !hasMoveUp}
-                  onClick={onReorderClick(index, index - 1)}
-                  uiSchema={uiSchema}
-                  registry={registry}
-                />
-              )}
-              {(hasMoveUp || hasMoveDown) && (
-                <MoveDownButton
-                  disabled={disabled || readonly || !hasMoveDown}
-                  onClick={onReorderClick(index, index + 1)}
-                  uiSchema={uiSchema}
-                  registry={registry}
-                />
-              )}
-              {hasCopy && (
-                <CopyButton
-                  disabled={disabled || readonly}
-                  onClick={onCopyIndexClick(index)}
-                  uiSchema={uiSchema}
-                  registry={registry}
-                />
-              )}
-              {hasRemove && (
-                <RemoveButton
-                  disabled={disabled || readonly}
-                  onClick={onDropIndexClick(index)}
-                  uiSchema={uiSchema}
-                  registry={registry}
-                />
-              )}
+              <ArrayFieldItemButtonsTemplate {...buttonsProps} />
             </div>
           )}
         </div>
