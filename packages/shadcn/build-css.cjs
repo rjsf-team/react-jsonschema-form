325a75f8f9707c31e68fb23d49d58001cb7c01bb
tuanminh <tuanphung2308@gmail.com>
2025-03-14 18:00:11 +0700

    feat: grid template implementation

--- packages/shadcn/build-css.cjs
+++ packages/shadcn/build-css.cjs
@@ -14,9 +14,12 @@ fs.readdirSync(srcDir).forEach((file) => {
     const distFile = path.join(distDir, file); // Keep the same filename
     try {
       // Add --content flag to explicitly process all content files
-      execSync(`npx tailwindcss -i ${srcFile} -o ${distFile} --minify --content="./src/**/*.{js,jsx,ts,tsx}"`, {
-        stdio: 'ignore',
-      });
+      execSync(
+        `npx tailwindcss -i ${srcFile} -o ${distFile} --minify --content="./src/**/*.{js,jsx,ts,tsx},../playground/src/**/*.{js,jsx,ts,tsx}"`,
+        {
+          stdio: 'ignore',
+        }
+      );
     } catch (error) {
       console.error(`Error building ${srcFile}:`, error);
       process.exit(1); // Exit with error code if any file fails
