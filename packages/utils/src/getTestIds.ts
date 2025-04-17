674c20e91144321e09980a362fd44f16fb9fcdab
Heath Chiavettone <heath.chiavettone@freenome.com>
2025-04-17 14:43:11 -0700

    Fixed #4564 by checking process type Fixed #4564 to eliminate typing issue

--- packages/utils/src/getTestIds.ts
+++ packages/utils/src/getTestIds.ts
@@ -27,7 +27,7 @@ export function getTestIdsBrowser(): TestIdShape {
 export default function getTestIds(): TestIdShape {
   // For some reason, even though process.env contains the value of `test` for NODE_ENV, accessing it directly returns
   // 'development'. Using `get()` does, in fact, return test so sticking with it
-  if (get(process, 'env.NODE_ENV') !== 'test') {
+  if (typeof process !== 'undefined' && get(process, 'env.NODE_ENV') !== 'test') {
     return getTestIdsBrowser();
   }
 
