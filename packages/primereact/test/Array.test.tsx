fa60ac9a63e312dcb5c4c1a40d9470eb8ed06ec8
arnold <arnold@jasny.net>
2025-04-26 21:01:21 +0200

    Fix tests for primereact. Mock inject stylesheet. Mock dropdown, multiselect and slider. They don't work with jsdom. Update snapshots.

--- packages/primereact/test/Array.test.tsx
+++ packages/primereact/test/Array.test.tsx
@@ -2,4 +2,17 @@ import { arrayTests } from '@rjsf/snapshot-tests';
 
 import Form from '../src';
 
+// Mock PrimeReact components that use parentElement or real DOM
+jest.mock('primereact/dropdown', () => ({
+  Dropdown: (props: Record<string, any>) => <select {...props} />,
+}));
+
+jest.mock('primereact/multiselect', () => ({
+  MultiSelect: (props: Record<string, any>) => <select multiple {...props} />,
+}));
+
+jest.mock('primereact/slider', () => ({
+  Slider: (props: Record<string, any>) => <input type='range' {...props} />,
+}));
+
 arrayTests(Form);
--- packages/primereact/test/GridSnap.test.tsx
+++ packages/primereact/test/GridSnap.test.tsx
@@ -2,6 +2,19 @@ import { gridTests } from '@rjsf/snapshot-tests';
 
 import Form from '../src';
 
+// Mock PrimeReact components that use parentElement or real DOM
+jest.mock('primereact/dropdown', () => ({
+  Dropdown: (props: Record<string, any>) => <select {...props} />,
+}));
+
+jest.mock('primereact/multiselect', () => ({
+  MultiSelect: (props: Record<string, any>) => <select multiple {...props} />,
+}));
+
+jest.mock('primereact/slider', () => ({
+  Slider: (props: Record<string, any>) => <input type='range' {...props} />,
+}));
+
 gridTests(Form, {
   ColumnWidthAll: { xs: 12 },
   ColumnWidth4: { xs: 4 },
