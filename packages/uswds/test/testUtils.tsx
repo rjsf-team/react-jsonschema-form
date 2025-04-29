7d632d2b46f5bcdd3dc80c77e4b1388b5e735ab8
John Jediny <JJediny@users.noreply.github.com>
2025-04-29 09:56:13 -0400

    add Utils test

--- packages/uswds/test/testUtils.tsx
+++ packages/uswds/test/testUtils.tsx
@@ -1,13 +1,27 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { FormProps } from "@rjsf/utils";
-import validator from "@rjsf/validator-ajv8";
+import { render } from '@testing-library/react';
+import { FormProps, RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
 
-import Theme from "../src/Theme"; // Import the USWDS theme
-import Form from "../src/Form"; // Import the USWDS Form
+import Theme from '../src/Theme';
+import Form from '../src/Form';
 
 export function createFormComponent<T = any>(props: FormProps<T>) {
-  return render(<Form validator={validator} {...props} theme={Theme} />);
+  const { schema, uiSchema, formData, ...rest } = props;
+  return render(
+    <Form
+      schema={schema}
+      uiSchema={uiSchema}
+      formData={formData}
+      validator={validator}
+      theme={Theme} // Pass the theme explicitly if needed
+      {...rest}
+    />,
+  );
 }
 
-// You can add other common test utilities here if needed
+export function createSandbox() {
+  const schema: RJSFSchema = {
+    type: 'string',
+  };
+  return createFormComponent({ schema });
+}
