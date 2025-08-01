0754d82ce006d18144cb02f647fc06509999b31a
Algirdyz <sdkster@gmail.com>
2025-08-02 01:26:40 +0200

    Update packages/mantine/src/templates/GridTemplate.tsx
    Co-authored-by: Nick Grosenbacher <nickgrosenbacher@gmail.com>

--- packages/mantine/src/templates/GridTemplate.tsx
+++ packages/mantine/src/templates/GridTemplate.tsx
@@ -4,7 +4,7 @@ import { GridTemplateProps } from '@rjsf/utils';
 /** Renders a `GridTemplate` for mantine, which is expecting the column sizing information coming in via the
  * extra props provided by the caller, which are spread directly on the `Grid`/`Grid.Col`.
  *
- * @param props - The GridTemplateProps, including the extra props containing the chakra-ui grid positioning details
+ * @param props - The GridTemplateProps, including the extra props containing the Mantine grid positioning details
  */
 export default function GridTemplate(props: GridTemplateProps) {
   const { children, column, fluid = true, ...rest } = props;
