1bc64ef93dea540ea4847e54807461edc7fbe5d9
Heath Chiavettone <heath.chiavettone@freenome.com>
2025-10-12 23:12:25 -0700

    Feature - Improve anyOf/oneOf rendering for optional data controls Improved the `anyOf`/`oneOf` rendering for optional data controls - In `@rjsf/core`, updated the `SchemaField` to optimize the `anyOf`/`oneOf` rendering along with hiding the label when there is no form data - In `snapshots`, updated `formTests.tsx` a new `optionalArrayWithAnyofs` property to the schema along with removing the `uiSchema` override for `optionalObjectWithOneofs`   - Updated the snapshots in all of the themes accordingly - In `playground`, made the same changes as in the `formTests.tsx` for the `optionalDataControls.ts` sample, moving the `experimental_defaultFormStateBehavior` into `liveSettings`   - Updated the `Sample` and `LiveSettings` types to support the `liveSettings` inside of a sample   - Updated the `Playground`'s `onSampleSelected` callback to merge any `liveSettings` in the sample on top of those already used in the playground - Updated the `CHANGELOG.md` accordingly

--- packages/playground/src/samples/Sample.ts
+++ packages/playground/src/samples/Sample.ts
@@ -1,8 +1,11 @@
 import { UiSchema } from '@rjsf/utils';
 import { FormProps } from '@rjsf/core';
 
+import { LiveSettings } from '../components/Header';
+
 export type UiSchemaForTheme = (theme: string) => UiSchema;
 
 export interface Sample extends Omit<FormProps, 'validator' | 'uiSchema'> {
   uiSchema?: FormProps['uiSchema'] | UiSchemaForTheme;
+  liveSettings?: LiveSettings;
 }
