64faa8d6e8d62ac83baf4da2a210be40ec7487e0
Suyog241005 <166608895+Suyog241005@users.noreply.github.com>
2025-11-02 09:45:57 +0530

    Update packages/snapshot-tests/src/formTests.tsx
    Co-authored-by: Heath C <51679588+heath-freenome@users.noreply.github.com>

--- packages/snapshot-tests/src/formTests.tsx
+++ packages/snapshot-tests/src/formTests.tsx
@@ -364,7 +364,6 @@ export function formTests(Form: ComponentType<FormProps>, customOptions: FormRen
       };
       const uiSchema: UiSchema = {
         'ui:widget': 'checkbox',
-        'ui:description': 'This is a field template description',
       };
       const tree = renderer.create(<Form schema={schema} uiSchema={uiSchema} validator={validator} />).toJSON();
       expect(tree).toMatchSnapshot();
