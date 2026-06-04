f3fe32d152427ce4aa7e10c1605938783350b3b9
Heath Chiavettone <vballdemigod@gmail.com>
2026-06-03 19:47:08 -0700

    - Responded to reviewer feedback

--- packages/playground/src/components/Playground.tsx
+++ packages/playground/src/components/Playground.tsx
@@ -158,7 +158,6 @@ export default function Playground({ themes, validators }: PlaygroundProps) {
         setLoaded(true);
       } catch (error) {
         alert('Unable to load form setup data.');
-        // oxlint-disable-next-line no-console
         console.error(error);
       }
 
@@ -175,7 +174,6 @@ export default function Playground({ themes, validators }: PlaygroundProps) {
     (event: IChangeEvent, id?: string) => {
       const { formData: newFormData } = event;
       if (id) {
-        // oxlint-disable-next-line no-console
         console.log('Field changed, id: ', id);
       }
 
@@ -186,9 +184,7 @@ export default function Playground({ themes, validators }: PlaygroundProps) {
   );
 
   const onFormDataSubmit = useCallback(({ formData: submittedFormData }: IChangeEvent, event: FormEvent<any>) => {
-    // oxlint-disable-next-line no-console
     console.log('submitted formData', submittedFormData);
-    // oxlint-disable-next-line no-console
     console.log('submit event', event);
     window.alert('Form submitted');
   }, []);
@@ -247,11 +243,8 @@ export default function Playground({ themes, validators }: PlaygroundProps) {
                 validator={validators[validator]}
                 onChange={onFormDataChange}
                 onSubmit={onFormDataSubmit}
-                // oxlint-disable-next-line no-console
                 onBlur={(id: string, value: string) => console.log(`Blurred ${id} with value ${value}`)}
-                // oxlint-disable-next-line no-console
                 onFocus={(id: string, value: string) => console.log(`Focused ${id} with value ${value}`)}
-                // oxlint-disable-next-line no-console
                 onError={(errorList: RJSFValidationError[]) => console.log('errors', errorList)}
                 ref={playGroundFormRef}
               />
