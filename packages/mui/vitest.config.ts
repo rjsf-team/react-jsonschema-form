97d8bacca0f46f8d44ebe84df067ccbb10b6f38f
Heath Chiavettone <vballdemigod@gmail.com>
2026-06-23 08:26:32 -0700

    - Update to Mui 9.1.2

--- packages/mui/vitest.config.ts
+++ packages/mui/vitest.config.ts
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'react-transition-group/TransitionGroupContext':
-        'react-transition-group/cjs/TransitionGroupContext.js',
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['../../testing/testSetup.ts', './test/setup-vitest.ts'],
     coverage: {
       provider: 'v8',
-    },
-    server: {
-      deps: {
-        inline: [/@mui\//],
-      },
     },
   },
 });
