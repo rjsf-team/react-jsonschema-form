bb68f0fbc12f60afc441bb5f0e10f556b5c1c3cb
Rob <robwalker@pursuit.org>
2026-08-23 15:48:14 -0400

    Fix premature FancySelect validation border

--- packages/shadcn/src/components/ui/fancy-select.tsx
+++ packages/shadcn/src/components/ui/fancy-select.tsx
@@ -116,7 +116,6 @@ export function FancySelect({
         }}
         className={cn(
           'flex h-9 w-full items-center justify-between gap-2 whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
-          !selectedItem && required && 'border-red-500',
           disabled && 'opacity-50 cursor-not-allowed',
           className,
         )}
